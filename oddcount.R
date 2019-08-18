65a4682e5a120c747ca596c9c1ccea8b48ce2d55
fernal73 <linus.fernandes@gmail.com>
2019-08-18 15:28:31 +0530

    Added odd counter

---
+++ oddcount.R
@@ -0,0 +1,13 @@
+# counts the number of odd integers in x
+oddcount <- function( x) { 
+  k <- 0 # assign 0 to k 
+  for (n in x) { 
+    if (n %% 2 == 1) 
+      k <- k + 1 
+  # %% is the modulo operator 
+} 
+  return( k) 
+} 
+
+oddcount( c( 1,3,5)) 
+oddcount( c( 1,2,3,7,9))
