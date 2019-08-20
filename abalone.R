d5e67a846bf7dfcda58307c29c815011167cf934
fernal73 <linus.fernandes@gmail.com>
2019-08-20 21:10:32 +0530

    Fixed graph for overlapping

--- abalone.R
+++ abalone.R
@@ -6,7 +6,7 @@ grps [[gen]] <- which(aba == gen)
 abam <- aba[grps$M,] 
 abaf <- aba[grps$F,] 
 pdf("abalone.pdf")  #set graphical output file 
-plot( abam$Length, abam$Diameter)
+plot( abam$Length, abam$Diameter,xlab="Length",ylab="Diameter",xlim=c(0,0.8),ylim=c(0,0.7))
 par(new=TRUE) 
-plot( abaf$Length, abaf$Diameter, pch ="x")
+plot( abaf$Length, abaf$Diameter, pch ="x",xlab="Length",ylab="Diameter",xlim=c(0,0.8),ylim=c(0,0.7))
 dev.off()
