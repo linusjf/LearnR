9b351d1dd34a47f1976b71c715105e2767229045
fernal73 <linus.fernandes@gmail.com>
2019-08-20 10:21:54 +0530

    Added pred

--- findruns.R
+++ findruns.R
@@ -1,26 +1,25 @@
 #!/usr/bin/env Rscript
-findruns <- function(x,k) {
-  n <- length(x)
-  runs <- vector(length=n)
-  count <- 0
-  for (i in 1:(n - k + 1)) {
-    if (all(x [i:(i + k - 1)] == 1)) {
-     count <- count + 1
-      runs[count] <- i
+findruns <- function(x, k) {
+    n <- length(x)
+    runs <- vector(length = n)
+    count <- 0
+    for (i in 1:(n - k + 1)) {
+        if (all(x[i:(i + k - 1)] == 1)) {
+            count <- count + 1
+            runs[count] <- i
+        }
     }
-  }
-  if (count > 0) {  
-    runs <- runs[1:count] 
-  } else 
-    runs <- NULL
-  return (runs)
+    if (count > 0) {
+        runs <- runs[1:count]
+    } else runs <- NULL
+    return(runs)
 }
 
-y <- c(1,0,0,1,1,1,0,1,1)
-a <- findruns(y,2)
+y <- c(1, 0, 0, 1, 1, 1, 0, 1, 1)
+a <- findruns(y, 2)
 print(a)
-b <- findruns(y,3)
+b <- findruns(y, 3)
 print(b)
-c <- findruns(y,6)
+c <- findruns(y, 6)
 print(c)
 
