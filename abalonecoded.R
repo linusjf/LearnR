127fabbebd384978f4681d797f63b70ec99e89de
fernal73 <linus.fernandes@gmail.com>
2019-08-20 22:17:58 +0530

    removed unwanted code

--- abalonecoded.R
+++ abalonecoded.R
@@ -3,6 +3,5 @@ aba <-read.csv("abalone.data", header = T, as.is = T)
 pdf("abalonecoded.pdf")  #set graphical output file 
 pchvec <- ifelse(aba$Sex == "M","o",
                  ifelse(aba$Sex=="F","x","."))
-print(pchvec)
 plot( aba$Length, aba$Diameter,xlab="Length",ylab="Diameter",xlim=c(0,0.8),ylim=c(0,0.7),pch=pchvec)
 dev.off()
