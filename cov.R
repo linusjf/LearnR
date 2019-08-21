621105589f2b53eda6db3c7d686ab29361c7e43e
fernal73 <linus.fernandes@gmail.com>
2019-08-21 18:04:19 +0530

    Covariance script

---
+++ cov.R
@@ -0,0 +1,10 @@
+#!/usr/bin/env Rscript
+makecov <- function(rho, n) { 
+  m <- matrix( nrow = n, ncol = n)  
+  m <- ifelse( row( m) == col( m), 1, rho) 
+  return( m)  
+}
+acov <- makecov(0.5,4)
+print(acov)
+bcov <- makecov(0.34,15)
+print(bcov)
