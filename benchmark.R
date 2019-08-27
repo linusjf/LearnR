99535a015419285ddb3af438a1ca6fbb0900f558
fernal73 <linus.fernandes@gmail.com>
2019-08-27 15:44:05 +0530

    Added benchmarking of mean function

---
+++ benchmark.R
@@ -0,0 +1,16 @@
+#!/usr/bin/env Rscript
+library(compiler)
+library(rbenchmark)
+my_mean = function(x) {
+  total = 0
+  n = length(x)
+  for(i in 1:n)
+    total = total + x[i]/n
+  total
+}
+cmpmean <- cmpfun(my_mean)
+## Generate some data
+x = rnorm(10000)
+benchmark(my_mean(x), cmpmean(x), mean(x), 
+          columns=c("test", "elapsed", "relative"),
+          order="relative", replications=5000)
