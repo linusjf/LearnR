e66b2896cf6d76fb80d72500b8868539281592fa
fernal73 <linus.fernandes@gmail.com>
2019-08-27 17:05:18 +0530

    Added new script median.R. made mind.R executable

---
+++ median.R
@@ -0,0 +1,18 @@
+#!/usr/bin/env Rscript
+# 1: Create large dataset
+X = data.frame(matrix(rnorm(1e7),nrow=1e6))
+
+startTime <- proc.time()
+
+# 2: Find the median of each column using a single core
+r1 = lapply(X, median)
+endTime <- proc.time() - startTime
+print(endTime)
+startTime2 <- proc.time()
+
+# 3: Find the median of each column using many cores
+# XXX: Change to function from package
+r2 = parallel::mclapply(X, median) 
+# runs in serial on Windows
+endTime2 <- proc.time() - startTime2
+print(endTime2)
