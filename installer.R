884740eab51ccbb48971a0d28e2802243b2d1699
fernal73 <linus.fernandes@gmail.com>
2020-01-17 14:11:19 +0530

    installer complete

--- installer.R
+++ installer.R
@@ -6,8 +6,23 @@ install_tools <- function() {
   install.packages("formatR", .libPaths()[1])
 }
 
+install_programs <- function() {
+  install.packages("stargazer", .libPaths()[1])
+  install.packages("compiler", .libPaths()[1])
+  install.packages("rbenchmark", .libPaths()[1])
+  install.packages("pixmap", .libPaths()[1])
+}
+
+install_rbyexample <- function() {
+  install.packages("stringi", .libPaths()[1])
+  install.packages("xtable", .libPaths()[1])
+  install.packages("tseries", .libPaths()[1])
+}
+
 main <- function(argv) {
   install_tools()
+  install_programs()
+  install_rbyexample()
 }
 
 if (identical(environment(), globalenv())) {
