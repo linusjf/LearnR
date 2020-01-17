0fea39c1e48ac0cfef7aeede8c4b912746654624
fernal73 <linus.fernandes@gmail.com>
2020-01-18 04:51:27 +0530

    Add main

--- RByExample/readdata.R
+++ RByExample/readdata.R
@@ -1,5 +1,5 @@
 #!/usr/bin/env Rscript
-library(Hmisc)
+suppressMessages(library(Hmisc))
 # Goal: To read in a simple data file, and look around it's contents.
 
 # Suppose you have a file "x.data" which looks like this:
@@ -9,14 +9,17 @@ library(Hmisc)
 #        2000,1.1,13
 # To read it in --
 
+main <- function(argv) {
 a <- read.table("x.data",
   sep = ",",
   col.names = c("year", "my1", "my2")
 )
-nrow(a)
+cat("Number of rows:\n\n")
+print(nrow(a))
 # Count the rows in a
 
-summary(a$year)
+cat("Summary:\n\n")
+print(summary(a$year))
 # The column "year" in data frame a
 # is accessed as a$year
 
@@ -24,12 +27,23 @@ summary(a$year)
 a$newcol <- a$my1 + a$my2
 # Makes a new R object "newvar"
 newvar <- a$my1 - a$my2
+cat(newvar, "\n")
 # Removes the column "my1"
 a$my1 <- NULL
 
 # You might find these useful, to "look around" a dataset --
+cat("\nStructure:\n\n")
 str(a)
-summary(a)
+cat("\nSummary:\n\n")
+print(summary(a))
 # This requires that you've installed the Hmisc package
-Hmisc::contents(a)
-describe(a)
+cat("\nContents:\n\n")
+print(Hmisc::contents(a))
+cat("Describe:\n\n")
+print(Hmisc::describe(a))
+return(0)
+}
+
+if (identical(environment(), globalenv())) {
+  quit(status = main(commandArgs(trailingOnly = TRUE)))
+}
