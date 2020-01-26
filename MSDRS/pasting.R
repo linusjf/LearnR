4241186c79e4f41a8ffcba0ed40a06fc92154d8c
fernal73 <linus.fernandes@gmail.com>
2020-01-26 12:40:57 +0530

    Pasting

---
+++ MSDRS/pasting.R
@@ -0,0 +1,19 @@
+#!/usr/bin/env Rscript
+
+main <- function(argv) {
+  print(paste("Square", "Circle", "Triangle"))
+  print(paste("Square", "Circle", "Triangle", sep = "+"))
+  print(paste0("Square", "Circle", "Triangle"))
+
+  shapes <- c("Square", "Circle", "Triangle")
+  print(paste("My favorite shape is a", shapes))
+  two_cities <- c("best", "worst")
+  print(paste("It was the", two_cities, "of times."))
+  print(paste(shapes, collapse = " "))
+  print(nchar("Supercalifragilisticexpialidocious"))
+  return(0)
+}
+
+if (identical(environment(), globalenv())) {
+  quit(status = main(commandArgs(trailingOnly = TRUE)))
+}
