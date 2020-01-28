54f57169bcd8230d3e70e1371c350c9a6ae20f3c
fernal73 <linus.fernandes@gmail.com>
2020-01-28 07:05:30 +0530

    Style and lint

--- MSDRS/meming.R
+++ MSDRS/meming.R
@@ -3,22 +3,25 @@ library(magrittr)
 library(pryr)
 
 main <- function(argv) {
-print(pryr::mem_used())
-print(ls())
-print(pryr::object_size(argv))
-print(sapply(ls(),
-             function(x)
-               object.size(get(x))) %>%
-sort %>%
-tail(5))
-print(pryr::mem_used())
-print(pryr::mem_change(rm(argv)))
-print(pryr::mem_used())
-print(pryr::object_size(integer(0)))
-print(pryr::object_size(integer(1000)))
-print(pryr::object_size(numeric(1000)))
-print(str(.Machine))
-return(0)
+  print(pryr::mem_used())
+  print(ls())
+  print(pryr::object_size(argv))
+  print(sapply(
+    ls(),
+    function(x) {
+      object.size(get(x))
+    }
+  ) %>%
+    sort() %>%
+    tail(5))
+  print(pryr::mem_used())
+  print(pryr::mem_change(rm(argv)))
+  print(pryr::mem_used())
+  print(pryr::object_size(integer(0)))
+  print(pryr::object_size(integer(1000)))
+  print(pryr::object_size(numeric(1000)))
+  print(str(.Machine))
+  return(0)
 }
 
 if (identical(environment(), globalenv())) {
