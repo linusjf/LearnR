bb4c2ab9b28b7bc6a5993c6a877145de1f65697c
fernal73 <linus.fernandes@gmail.com>
2020-01-30 04:45:39 +0530

    Add walking.R

---
+++ MSDRS/walking.R
@@ -0,0 +1,14 @@
+#!/usr/bin/env Rscript
+library(purrr)
+
+main <- function(argv) {
+print(walk(c("Friends, Romans, countrymen,",
+       "lend me your ears;",
+       "I come to bury Caesar,",
+       "not to praise him."), message))
+return(0)
+}
+
+if (identical(environment(), globalenv())) {
+  quit(status = main(commandArgs(trailingOnly = TRUE)))
+}
