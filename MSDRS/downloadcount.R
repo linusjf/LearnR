51c031f20045fb91b770a4299c45e7e7a9e7dd02
fernal73 <linus.fernandes@gmail.com>
2020-01-29 08:50:56 +0530

    Modify downloadcount

--- MSDRS/downloadcount.R
+++ MSDRS/downloadcount.R
@@ -2,36 +2,47 @@
 suppressMessages(library(dplyr))
 library(readr)
 
-## pkgname: package name (character)
-## date: YYYY-MM-DD format (character)
-num_download <- function(pkgname, date) {
-  ## Construct web URL
+check_for_logfile <- function(date) {
   year <- substr(date, 1, 4)
   src <- sprintf(
     "http://cran-logs.rstudio.com/%s/%s.csv.gz",
     year, date
   )
-
-  ## Construct path for storing local file
   dest <- file.path(".", basename(src))
-
-  ## Don't download if the file is already there!
   if (!file.exists(dest)) {
-    download.file(src, dest, quiet = TRUE)
+    val <- download.file(src, dest, quiet = TRUE)
+    if (!val) {
+      stop("unable to download file ", src)
+    }
   }
+  return(dest)
+}
 
-  count <- readr::read_csv(dest,
-    col_types = "ccicccccci", progress =
-      FALSE
-  ) %>%
+## pkgname: package name (character)
+## date: YYYY-MM-DD format (character)
+num_download <- function(pkgname, date = "2016-07-20") {
+  check_pkg_deps()
+  dest <- check_for_logfile(date)
+  count <- read_csv(dest, col_types = "ccicccccci", progress = FALSE) %>%
     filter(package == pkgname) %>%
     nrow()
   return(count)
 }
 
+check_pkg_deps <- function() {
+  if (!require(readr)) {
+    message("installing the 'readr' package")
+    install.packages("readr")
+  }
+  if (!require(dplyr)) {
+    stop("the 'dplyr' package needs to be installed first")
+  }
+}
+
 main <- function(argv) {
   print(num_download("filehash", "2016-07-20"))
   print(num_download("Rcpp", "2016-07-19"))
+  print(num_download("Rcpp"))
   return(0)
 }
 
