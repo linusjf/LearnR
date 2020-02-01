702424867629716075e4ef8320dc9bbc9a69a8f4
fernal73 <linus.fernandes@gmail.com>
2020-02-01 22:33:49 +0530

    add debugging example

---
+++ MSDRS/debugging.R
@@ -0,0 +1,15 @@
+#!/usr/bin/env Rscript
+check_n_value <- function(n) {
+        if (n > 0) {
+                stop("n should be <= 0")
+        }
+}
+
+# nolint start
+error_if_n_is_greater_than_zero <- function(n) {
+        check_n_value(n)
+        return(n)
+}
+# nolint end
+
+error_if_n_is_greater_than_zero(5)
