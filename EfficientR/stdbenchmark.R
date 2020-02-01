b9fbcb23a7aaec97b6f34d90ec23c9456f6d4e2d
fernal73 <linus.fernandes@gmail.com>
2020-02-01 06:01:23 +0530

    Add benchmarking

---
+++ EfficientR/stdbenchmark.R
@@ -0,0 +1,11 @@
+#!/usr/bin/env Rscript
+library(benchmarkme)
+
+main <- function(argv) {
+print(benchmarkme::benchmark_std())
+return(0)
+}
+
+if (identical(environment(), globalenv())) {
+  quit(status = main(commandArgs(trailingOnly = TRUE)))
+}
