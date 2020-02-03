6d2f410766f72edb81d472a879ea63e5bb1aff04
fernal73 <linus.fernandes@gmail.com>
2020-02-03 06:26:09 +0530

    Add internalcmethods

---
+++ EfficientR/internalcmethods.R
@@ -0,0 +1,10 @@
+#!/usr/bin/env Rscript
+
+main <- function(argv) {
+print(names(methods:::.BasicFunsList))
+return(0)
+}
+
+if (identical(environment(), globalenv())) {
+  quit(status = main(commandArgs(trailingOnly = TRUE)))
+}
