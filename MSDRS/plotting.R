8788b897d053750b1cf1e0fa7e991209742e1c52
fernal73 <linus.fernandes@gmail.com>
2020-02-05 12:09:51 +0530

    Add plots

--- MSDRS/plotting.R
+++ MSDRS/plotting.R
@@ -1,13 +1,21 @@
 #!/usr/bin/env Rscript
 library(titanic)
-data("titanic_train", package = "titanic")
+data("titanic_train")
 library(faraway)
 data("worldcup")
 library(ggplot2)
 
 main <- function(argv) {
-  titanic <- titanic_train
-
+ggplot(data = titanic_train) +
+       geom_histogram(aes(x = Fare), bins = 15)
+ggplot2::ggsave("titanicfarehist.pdf")
+ggplot(worldcup, aes(x = Time, y = Passes)) +
+  geom_point()
+ggplot2::ggsave("wctimepasses.pdf")
+ggplot(worldcup, aes(x = Time, y = Passes,
+                     color = Position, size = Shots)) +
+  geom_point()
+ggplot2::ggsave("wctimepassescolor.pdf")
   return(0)
 }
 
