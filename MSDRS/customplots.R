c464dd35edee4831e4c89ac19cd85253afaee105
fernal73 <linus.fernandes@gmail.com>
2020-02-07 13:14:47 +0530

    Draw custom plots

--- MSDRS/customplots.R
+++ MSDRS/customplots.R
@@ -10,12 +10,22 @@ suppressMessages(library(dlnm))
 data(chicagoNMMAPS)
 
 main <- function(argv) {
-chic <- dlnm::chicagoNMMAPS
-print(head(chic))
-chic_july <- chic %>%
-  filter(month == 7 & year == 1995)
-print(head(chic_july))
-return(0)
+  chic <- dlnm::chicagoNMMAPS
+  print(head(chic))
+  chic_july <- chic %>%
+    filter(month == 7 & year == 1995)
+  print(head(chic_july))
+  plots <- list()
+  plots[[1]] <- ggplot(worldcup, aes(x = Time, y = Shots)) +
+    geom_point() +
+    theme_classic()
+  plots[[2]] <- ggplot(worldcup, aes(x = Time, y = Shots)) +
+    geom_point() +
+    theme_tufte()
+  pdf("customplots.pdf")
+  invisible(lapply(plots, print))
+  graphics.off()
+  return(0)
 }
 
 if (identical(environment(), globalenv())) {
