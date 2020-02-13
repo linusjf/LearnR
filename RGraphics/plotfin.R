2f1c7681ce44d2ad28e0e07b82957af8651d6267
fernal73 <linus.fernandes@gmail.com>
2020-02-13 20:21:26 +0530

    Add projections and finance plotting example

---
+++ RGraphics/plotfin.R
@@ -0,0 +1,13 @@
+#!/usr/bin/env Rscript
+suppressMessages(library(quantmod))
+
+main <- function(argv) {
+  quantmod::getSymbols("AAPL")
+chartSeries(AAPL, subset = "last 4 months"
+ )
+return(0)
+}
+
+if (identical(environment(), globalenv())) {
+  quit(status = main(commandArgs(trailingOnly = TRUE)))
+}
