a939861ac1b1bc18b57e199aa143839d85158103
fernal73 <linus.fernandes@gmail.com>
2020-02-20 19:08:07 +0530

    residuals object

--- RGraphics/Chapter02/lm.R
+++ RGraphics/Chapter02/lm.R
@@ -3,6 +3,8 @@ library(grid)
 
 main <- function(argv) {
   print(head(LifeCycleSavings))
+  print(LifeCycleSavings[c("Libya", "Zambia"),])
+  print(summary(LifeCycleSavings))
   par(mfrow = c(3, 2), cex = 0.6, mar = c(4, 4, 4, 2), mex = 0.8)
   plot(lm_sr <- lm(sr ~ pop15 + pop75 + dpi + ddpi, data = LifeCycleSavings),
     id.n = 1, cex.caption = 0.8, which = 1:6,
@@ -13,6 +15,16 @@ main <- function(argv) {
   pairs(LifeCycleSavings,
     panel = panel.smooth,
     main = "LifeCycleSavings data"
+  )
+  print(summary(lm_sr))
+  print(resid(lm_sr))
+  print("Model given level of significance = 0.05")
+  par(mfrow = c(3, 2), cex = 0.6, mar = c(4, 4, 4, 2), mex = 0.8)
+  plot(lm_sr <- lm(sr ~ pop15 + ddpi, data = LifeCycleSavings),
+    id.n = 1, cex.caption = 0.8, which = 1:6,
+    panel = function(...) {
+      panel.smooth(..., col.smooth = "gray")
+    }
   )
   print(summary(lm_sr))
   print("Model given level of significance = 0.01")
