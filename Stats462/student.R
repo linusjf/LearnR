3bf9bd3e901722b952d8fb95f53c2febb76a903e
fernal73 <linus.fernandes@gmail.com>
2020-03-03 12:51:09 +0530

    Add student heights weights regression

---
+++ Stats462/student.R
@@ -0,0 +1,46 @@
+#!/usr/bin/env Rscript
+library(skimr)
+
+main <- function(argv) {
+  data <- read.table("student_height_weight.txt",
+    header = TRUE
+  )
+  print(head(data))
+  print(skimr::skim(data))
+  reg <- lm(wt ~ ht, data = data)
+  print(reg)
+  new_data <- data.frame(
+    ht = c(66, 67, 68, 69, 70, 71)
+  )
+  values <- predict(reg, newdata = new_data)
+  print(diff(values))
+  plot_student(data, reg)
+  return(0)
+}
+
+plot_student <- function(data, reg) {
+  par(mar = c(4, 7, 4, 1))
+  plot(data$ht, data$wt,
+    main = "Height versus weight",
+    xlab = "height", ylab = "weight",
+    pch = 19, frame = FALSE
+  )
+  abline(reg, col = "blue")
+  x0 <- mean(data$ht)
+  y0 <- mean(data$wt)
+  x1 <- x0 + 2
+  y1 <- y0 + 25
+  arrows(x1, y1, x0, y0,
+    angle = 30, code = 2, col = "black", lwd = 4
+  )
+  coefs <- reg$coefficients
+  text(x1, y1 + 3, paste(
+    coefs["(Intercept)"],
+    " + \n",
+    coefs["ht"], "ht"
+  ))
+}
+
+if (identical(environment(), globalenv())) {
+  quit(status = main(commandArgs(trailingOnly = TRUE)))
+}
