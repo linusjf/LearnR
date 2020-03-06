520ac70400c795eedf7b1f6522b9b1a39c20b555
fernal73 <linus.fernandes@gmail.com>
2020-03-06 14:21:54 +0530

    Sign Distance at 99%

---
+++ Stats462/Lesson03/signdist99.R
@@ -0,0 +1,44 @@
+#!/usr/bin/env Rscript
+library(skimr)
+
+main <- function(argv) {
+  data <- read.table("../Data/signdist.txt",
+    header = TRUE
+  )
+  print(head(data))
+  print(skimr::skim(data))
+  reg <- lm(Distance ~ Age, data = data)
+  print(reg)
+  print(summary(reg))
+  print(confint(reg, level = 0.99))
+  plot_signdist(data, reg)
+  return(0)
+}
+
+plot_signdist <- function(data, reg) {
+  par(mar = c(4, 4, 7, 1))
+  coefs <- reg$coefficients
+  main_label <- paste(
+    "Age versus Sight Distance\n",
+    coefs["(Intercept)"],
+    coefs["Age"], "Age"
+  )
+  plot(data$Age, data$Distance,
+    main = main_label,
+    xlab = "Age", ylab = "Sight Distance",
+    pch = 19, frame = FALSE
+  )
+  abline(reg, col = "blue")
+  box(which = "plot", lty = "solid")
+  summ <- summary(reg)
+  legends <- c(
+    paste0("S - ", format(summ$sigma, digits = 4)),
+    paste0("Rsq - ", format(summ$r.squared, digits = 4)),
+    paste0("Rsq(adj) - ", format(summ$adj.r.squared, digits = 4))
+  )
+  legend("topright", legends)
+}
+
+if (identical(environment(), globalenv())) {
+  quit(status = main(commandArgs(trailingOnly = TRUE)))
+}
