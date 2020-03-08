45031f794298abea402ac974676fb0667653ce47
fernal73 <linus.fernandes@gmail.com>
2020-03-08 07:59:17 +0530

    Remove zeros from dataset. use log transform now

--- CoronaVirus/mapworld.R
+++ CoronaVirus/mapworld.R
@@ -14,17 +14,20 @@ main <- function(argv) {
   download.file(datafileurl, "confirmed.csv")
   datacov <- readr::read_csv("confirmed.csv",
   skip_empty_rows = TRUE)
+  yday <- Sys.Date() - 1
+  yday <- format(yday, "%m/%d/%y")
+  yday <- gsub("^0", "", yday)
+  ydaystring <- gsub("/0", "/", yday)
+  yday <- paste0("`", ydaystring, "`")
+  print(yday)
+ datacov[datacov == 0] <- NA
+ datacov <- tidyr::drop_na(datacov, all_of(ydaystring))
+ print(nrow(datacov))
   ## get the world map
   world <- ggplot2::map_data("world")
 
   # cutoffs based on the number of cases
   mybreaks <- c(1, 20, 100, 1000, 50000)
-  yday <- Sys.Date() - 1
-  yday <- format(yday, "%m/%d/%y")
-  yday <- gsub("^0", "", yday)
-  yday <- gsub("/0", "/", yday)
-  yday <- paste0("`", yday, "`")
-  print(yday)
 
   ggplot() +
     geom_polygon(
@@ -36,7 +39,7 @@ main <- function(argv) {
         eval(parse(text = yday))
     ), stroke = F, alpha = 0.7) +
     scale_size_continuous(
-      name = "Cases", trans = "sqrt", range = c(1, 7), breaks = mybreaks,
+      name = "Cases", trans = "log", range = c(1, 7), breaks = mybreaks,
       labels = c(
         "1-19", "20-99", "100-999", "1,000-49,999",
         "50,000+"
@@ -45,7 +48,7 @@ main <- function(argv) {
     # scale_alpha_continuous(name="Cases", trans="log", range=c(0.1,
     # 0.9),breaks=mybreaks) +
     scale_color_viridis_c(
-      option = "inferno", name = "Cases", trans = "sqrt",
+      option = "inferno", name = "Cases", trans = "log",
       breaks = mybreaks, labels = c(
         "1-19", "20-99",
         "100-999",
