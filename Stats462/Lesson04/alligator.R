b617ca0e8494856277317cb9c8f4a93fd5f20062
fernal73 <linus.fernandes@gmail.com>
2020-03-10 07:09:23 +0530

    Add visual linearity tests examples

---
+++ Stats462/Lesson04/alligator.R
@@ -0,0 +1,55 @@
+#!/usr/bin/env Rscript
+library(skimr)
+
+main <- function(argv) {
+  data <- read.table("../Data/alligator.txt",
+    header = TRUE
+  )
+  print(head(data))
+  print(skimr::skim(data))
+
+  process_alligator(data)
+
+  return(0)
+}
+
+process_alligator <- function(data) {
+  reg <- lm(weight ~ length, data = data)
+  print(reg)
+  print(summary(reg))
+  print(confint(reg))
+  plot_alligator(data, reg)
+}
+
+plot_alligator <- function(data, reg) {
+  par(mar = c(4, 4, 5, 1))
+  coefs <- reg$coefficients
+  main_label <- paste(
+    "Length versus Weight\n",
+    coefs["(Intercept)"],
+    x <- if (sign(coefs["length"]) == 1) {
+      "+"
+    } else {
+      "-"
+    },
+    abs(coefs["length"]), "Length"
+  )
+  plot(data$length, data$weight,
+    main = main_label,
+    xlab = "Length", ylab = "Weight",
+    pch = 19, frame = TRUE
+  )
+  abline(reg, col = "blue")
+  summ <- summary(reg)
+  legends <- c(
+    paste0("S - ", format(summ$sigma, digits = 4)),
+    paste0("Rsq - ", format(summ$r.squared, digits = 4)),
+    paste0("Rsq(adj) - ", format(summ$adj.r.squared, digits = 4))
+  )
+  legend("topleft", legends)
+  abline(h = mean(data$weight), col = "black", lty = "dashed")
+}
+
+if (identical(environment(), globalenv())) {
+  quit(status = main(commandArgs(trailingOnly = TRUE)))
+}
---
+++ Stats462/Lesson04/corrosion.R
@@ -0,0 +1,55 @@
+#!/usr/bin/env Rscript
+library(skimr)
+
+main <- function(argv) {
+  data <- read.table("../Data/corrosion.txt",
+    header = TRUE
+  )
+  print(head(data))
+  print(skimr::skim(data))
+
+  process_corrosion(data)
+
+  return(0)
+}
+
+process_corrosion <- function(data) {
+  reg <- lm(wgtloss ~ iron, data = data)
+  print(reg)
+  print(summary(reg))
+  print(confint(reg))
+  plot_corrosion(data, reg)
+}
+
+plot_corrosion <- function(data, reg) {
+  par(mar = c(4, 4, 5, 1))
+  coefs <- reg$coefficients
+  main_label <- paste(
+    "Iron versus Weight Loss\n",
+    coefs["(Intercept)"],
+    x <- if (sign(coefs["iron"]) == 1) {
+      "+"
+    } else {
+      "-"
+    },
+    abs(coefs["iron"]), "Iron"
+  )
+  plot(data$iron, data$wgtloss,
+    main = main_label,
+    xlab = "Iron", ylab = "Weight Loss",
+    pch = 19, frame = TRUE
+  )
+  abline(reg, col = "blue")
+  summ <- summary(reg)
+  legends <- c(
+    paste0("S - ", format(summ$sigma, digits = 4)),
+    paste0("Rsq - ", format(summ$r.squared, digits = 4)),
+    paste0("Rsq(adj) - ", format(summ$adj.r.squared, digits = 4))
+  )
+  legend("topright", legends)
+  abline(h = mean(data$wgtloss), col = "black", lty = "dashed")
+}
+
+if (identical(environment(), globalenv())) {
+  quit(status = main(commandArgs(trailingOnly = TRUE)))
+}
