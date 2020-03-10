2085716d9e2663d4fcccc98bac8f52f0bf88759e
fernal73 <linus.fernandes@gmail.com>
2020-03-10 06:42:34 +0530

    update latestdata.R

--- CoronaVirus/latestdata.R
+++ CoronaVirus/latestdata.R
@@ -5,16 +5,9 @@ main <- function(argv) {
   x <- nCov2019::get_nCov2019(lang = "en")
   print(x)
   cat("Global data:\n")
-  print(x["global", ])
-  cat("China provincial data:\n")
-  print(x[])
-  cat("Hubei data:\n")
-  print(x["Hubei", ])
+  print(head(x["global", ], 30))
   print(summary(x))
 
-  cat("Historical data:\n")
-  history <- nCov2019::load_nCov2019(lang = "en")
-  print(history)
   return(0)
 }
 
