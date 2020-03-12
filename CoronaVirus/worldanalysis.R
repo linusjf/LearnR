fd83ca712bd27c254b8446d150f7303dc3608c82
fernal73 <linus.fernandes@gmail.com>
2020-03-12 09:11:47 +0530

    Add top10 graph

--- CoronaVirus/worldanalysis.R
+++ CoronaVirus/worldanalysis.R
@@ -48,26 +48,28 @@ download_csv <- function(filename) {
 
 ## data cleaning and transformation
 clean_data <- function(data) {
-## remove some columns
-data %<>%
-  select(-c(Province.State, Lat, Long)) %>%
-  rename(country = Country.Region)
-## convert from wide to long format
-data %<>%
-  gather(key = date,
-         value = count,
-         -country, na.rm = TRUE)
-## convert from character to date
-data %<>%
-  mutate(date = date %>%
-         substr(2, 8) %>%
-         mdy())
-## aggregate by country
-data %<>%
-  group_by(country, date) %>%
-  summarise(count = sum(count)) %>%
-  as.data.frame()
-return(data)
+  ## remove some columns
+  data %<>%
+    select(-c(Province.State, Lat, Long)) %>%
+    rename(country = Country.Region)
+  ## convert from wide to long format
+  data %<>%
+    gather(
+      key = date,
+      value = count,
+      -country, na.rm = TRUE
+    )
+  ## convert from character to date
+  data %<>%
+    mutate(date = date %>%
+      substr(2, 8) %>%
+      mdy())
+  ## aggregate by country
+  data %<>%
+    group_by(country, date) %>%
+    summarise(count = sum(count)) %>%
+    as.data.frame()
+  return(data)
 }
 
 main <- function(argv) {
@@ -110,74 +112,159 @@ main <- function(argv) {
     merge(data_deaths) %>%
     merge(data_recovered)
   ## counts for the whole world
-data_world <- data %>%
-  group_by(date) %>%
-summarise(country = "World",
-confirmed = sum(confirmed),
-deaths = sum(deaths),
-recovered = sum(recovered))
-data %<>%
-  rbind(data_world)
-## remaining confirmed cases
-data %<>%
-  mutate(remaining.confirmed = confirmed - deaths - recovered)
-write.csv(data, "summarised.csv")
-data %<>% add_rates()
-write.csv(data, "rates.csv")
+  data_world <- data %>%
+    group_by(date) %>%
+    summarise(
+      country = "World",
+      confirmed = sum(confirmed),
+      deaths = sum(deaths),
+      recovered = sum(recovered)
+    )
+  data %<>%
+    rbind(data_world)
+  ## remaining confirmed cases
+  data %<>%
+    mutate(remaining.confirmed = confirmed - deaths - recovered)
+  write.csv(data, "summarised.csv")
+  data %<>%
+    add_rates()
+  write.csv(data, "rates.csv")
+  plot_top10_confirmed(data)
   return(0)
 }
 
 add_rates <- function(data) {
 
-## sort by country and date
-data %<>%
-  arrange(country, date)
-## daily increases of deaths and cured cases
-## set NA to the increases on day1
-day1 <- min(data$date)
-data %<>%
-  mutate(confirmed.inc =
-         ifelse(date == day1,
-                NA,
-                confirmed - lag(confirmed,
-                                n = 1)),
-deaths.inc =
-  ifelse(date == day1,
-         NA,
-         deaths - lag(deaths, n = 1)),
-recovered.inc =
-  ifelse(date == day1,
-         NA,
-         recovered - lag(
-                         recovered,
-                         n = 1)))
-## death rate based on total deaths and cured cases
-data %<>%
-  mutate(rate.upper =
-         (100 * deaths /
+  ## sort by country and date
+  data %<>%
+    arrange(country, date)
+  ## daily increases of deaths and cured cases
+  ## set NA to the increases on day1
+  day1 <- min(data$date)
+  data %<>%
+    mutate(
+      confirmed.inc =
+        ifelse(date == day1,
+          NA,
+          confirmed - lag(confirmed,
+            n = 1
+          )
+        ),
+      deaths.inc =
+        ifelse(date == day1,
+          NA,
+          deaths - lag(deaths, n = 1)
+        ),
+      recovered.inc =
+        ifelse(date == day1,
+          NA,
+          recovered - lag(
+            recovered,
+            n = 1
+          )
+        )
+    )
+  ## death rate based on total deaths and cured cases
+  data %<>%
+    mutate(
+      rate.upper =
+        (100 * deaths /
           (deaths + recovered)) %>%
-         round(1))
-## lower bound: death rate based on total confirmed cases
-data %<>%
-  mutate(rate.lower =
-         (100 * deaths /
+          round(1)
+    )
+  ## lower bound: death rate based on total confirmed cases
+  data %<>%
+    mutate(
+      rate.lower =
+        (100 * deaths /
           confirmed) %>%
-         round(1))
-## death rate based on the number of death/cured on every single day
-data %<>%
-  mutate(rate.daily =
-         (100 * deaths.inc /
+          round(1)
+    )
+  ## death rate based on the number of death/cured on every single day
+  data %<>%
+    mutate(
+      rate.daily =
+        (100 * deaths.inc /
           (deaths.inc +
-           recovered.inc)) %>%
-                 round(1))
-return(data)
+            recovered.inc)) %>%
+          round(1)
+    )
+  return(data)
 }
+
+plot_top10_confirmed <- function(data) {
+  max_date <- max(data$date)
+  print(max_date)
+  ## ranking by confirmed cases
+  data_latest <- data %>%
+    filter(date == max(date)) %>%
+    select(country, date, confirmed, deaths, recovered, remaining.confirmed) %>%
+    mutate(ranking = dense_rank(desc(confirmed)))
+  ## top 10 countries: 12 incl. 'World' and 'Others'
+  top_countries <- data_latest %>%
+    filter(ranking <= 12) %>%
+    arrange(ranking) %>%
+    pull(country) %>%
+    as.character()
+  ## move 'Others' to the end
+  top_countries %<>%
+    setdiff("Others") %>%
+    c("Others")
+  print(top_countries)
+
+  ## put all others in a single group of 'Others'
+  df <- data_latest %>%
+    filter(!is.na(country) & country != "World") %>%
+    mutate(
+      country =
+        ifelse(ranking <= 12,
+          as.character(country), "Others"
+        )
+    ) %>%
+    mutate(country = country %>%
+      factor(
+        levels =
+          c(top_countries)
+      ))
+  df %<>%
+    group_by(country) %>%
+    summarise(
+      confirmed =
+        sum(confirmed)
+    )
+  ## percentage and label
+  df %<>%
+    mutate(per = (100 * confirmed /
+      sum(confirmed)) %>%
+      round(1)) %>%
+    mutate(txt = paste0(
+      country,
+      ": ",
+      confirmed,
+      " (",
+      per,
+      "%)"
+    ))
+
+  df %>% ggplot(aes(fill = country)) +
+    geom_bar(aes(x = "", y = per), stat = "identity") +
+    coord_polar("y", start = 0) +
+    xlab("") +
+    ylab("Percentage (%)") +
+    labs(title = paste0(
+      "Top 10 Countries with Most Confirmed Cases (",
+      max_date, ")"
+    )) +
+    scale_fill_discrete(name = "Country", labels = df$txt)
+  ggplot2::ggsave("top10confirmed.pdf")
+}
+
 latest_date <- function(dates) {
   return(max(dates))
 }
 
 dates <- function(data) {
-  cols   <- ncol(data)
+  cols <- ncol(data)
   ## get dates from column names
   dates <- names(data)[5:cols] %>%
     substr(2, 8) %>%
@@ -186,7 +273,7 @@ dates <- function(data) {
 }
 
 date_range <- function(dates) {
- return(range(dates))
+  return(range(dates))
 }
 
 print_sample_data <- function(data, name) {
@@ -194,9 +281,10 @@ print_sample_data <- function(data, name) {
     sample_n(10) %>%
     kable("latex",
       booktabs = TRUE,
-      caption = paste("Raw Data (",
-                      name,
-                      "First 10 Columns only)"
+      caption = paste(
+        "Raw Data (",
+        name,
+        "First 10 Columns only)"
       )
     ) %>%
     kable_styling(font_size = 6, latex_options = c(
@@ -204,9 +292,13 @@ print_sample_data <- function(data, name) {
       "repeat_header"
     )) %>%
     landscape() %>%
-    save_kable(file = paste0(name,
-                             ".pdf"),
-    keep_tex = TRUE)
+    save_kable(
+      file = paste0(
+        name,
+        ".pdf"
+      ),
+      keep_tex = TRUE
+    )
 }
 
 if (identical(environment(), globalenv())) {
