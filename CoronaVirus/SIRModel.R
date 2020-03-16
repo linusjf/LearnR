73c5e8f0bd0669ac64b54e404e6d297370aece54
fernal73 <linus.fernandes@gmail.com>
2020-03-16 13:21:20 +0530

    Suppress plot warnings

--- CoronaVirus/SIRModel.R
+++ CoronaVirus/SIRModel.R
@@ -48,7 +48,7 @@ main <- function(argv) {
   
   data <- readr::read_csv("india_data.csv")
   infected <- data$confirmed
-  init <- c(S = N - infected[1], I = infected[1], R = 0)
+  init <- c(S = N_INDIA - infected[1], I = infected[1], R = 0)
   
   plot_data <- list(label = "\nSIR model 2019-nCoV India")
   analyse(init, infected, death_rate, recovery_rate, plot_data)
@@ -72,8 +72,8 @@ analyse <- function(init, infected, death_rate, recovery_rate, plot_data) {
   opt <- optim(c(0.5, recovery_rate), 
                RSS,
     method = "L-BFGS-B", 
-    lower = c(0,max(0, recovery_rate - 0.005)), upper =
-      c(1, min(recovery_rate + 0.005,1))
+    lower = c(0,max(0, recovery_rate - 0.01)), upper =
+      c(1, min(recovery_rate + 0.01,1))
   , infected = infected, init = init )
   print(opt$message)
 
@@ -102,8 +102,9 @@ analyse <- function(init, infected, death_rate, recovery_rate, plot_data) {
     lty = 1,
     lwd = 2, col = col, inset = 0.05
   )
-  matplot(fit$time, fit[, 2:4], type = "l", xlab = "Day", ylab = "Number of
-          subjects", lwd = 2, lty = 1, col = col, log = "y")
+  
+  suppressWarnings(matplot(fit$time, fit[, 2:4], type = "l", xlab = "Day", ylab = "Number of
+          subjects", lwd = 2, lty = 1, col = col, log = "y"))
 
   points(seq(length(infected)), infected)
   legend("bottomright", c("Susceptibles", "Infecteds", "Recovereds"),
@@ -119,16 +120,20 @@ analyse <- function(init, infected, death_rate, recovery_rate, plot_data) {
   # nolint end
   print(R0)
 
-  fit[fit$I == max(fit$I), "I", drop = FALSE] # height of pandemic
+  fit[fit$I == max(fit$I), "I", drop = FALSE]
+  # height of pandemic
 
   print(paste0("Maximum deaths: ",
-               max(fit$I) * death_rate))
+               round(max(fit$I) * death_rate)))
 
 }
 
 # nolint start
 # world population 7.7 billion
 N <- 7700000000
+# nolint end
+# india population 1.37 billion
+N_INDIA <- 1370000000
 # nolint end
 if (identical(environment(), globalenv())) {
   quit(status = main(commandArgs(trailingOnly = TRUE)))
