73e72baf2d2adc6acf362431acd9bbb6ea742330
fernal73 <linus.fernandes@gmail.com>
2020-03-16 16:41:58 +0530

    Lint and style

--- CoronaVirus/SIRModel.R
+++ CoronaVirus/SIRModel.R
@@ -4,12 +4,10 @@ library(readr)
 
 sir <- function(time, state, parameters) {
   par <- as.list(c(state, parameters))
-  with(par,{
-    ds <- -beta / N * I * S
-    di <- beta / N * I * S - gamma * I
-    dr <- gamma * I
-    list(c(ds, di, dr), N = N)
-  })
+    ds <- -par$beta / par$N * par$I * par$S
+    di <- par$beta / par$N * par$I * par$S - par$gamma * par$I
+    dr <- par$gamma * par$I
+    list(c(ds, di, dr), N = par$N)
 }
 
 rss <- function(parameters, infected = NULL, init = NULL, popn = NULL) {
@@ -42,8 +40,10 @@ main <- function(argv) {
   init <- c(S = population$World - infected[1], I = infected[1], R = 0)
 
   plot_data <- list(label = "\nSIR model 2019-nCoV World")
-  analyse(init, infected, death_rate, recovery_rate, deaths, population$World, plot_data)
-  analyse(init, infected, death_rate, upper_recovery_rate, deaths,population$World, plot_data)
+  analyse(init, infected, death_rate, recovery_rate, deaths, population$World,
+          plot_data)
+  analyse(init, infected, death_rate, upper_recovery_rate, deaths,
+          population$World, plot_data)
 
   data <- readr::read_csv("india_data.csv")
   infected <- data$confirmed
@@ -53,8 +53,10 @@ main <- function(argv) {
   init <- c(S = population$India - infected[1], I = infected[1], R = 0)
 
   plot_data <- list(label = "\nSIR model 2019-nCoV India")
-  analyse(init, infected, death_rate, recovery_rate, deaths,population$India, plot_data)
-  analyse(init, infected, death_rate, upper_recovery_rate, deaths,population$India, plot_data)
+  analyse(init, infected, death_rate, recovery_rate, deaths, population$India,
+          plot_data)
+  analyse(init, infected, death_rate, upper_recovery_rate, deaths,
+          population$India, plot_data)
   recovery_rate <- last_record$recovered / last_record$confirmed
   death_rate <- last_record$deaths / last_record$confirmed
   upper_recovery_rate <- 1 - death_rate
@@ -62,10 +64,14 @@ main <- function(argv) {
   print("Analysing with India specific rates")
   print(paste("India Death rate: ", death_rate))
   print(paste("India Recovery rate: ", recovery_rate))
-  analyse(init, infected, death_rate, recovery_rate, deaths,population$India,
-          plot_data)
-  analyse(init, infected, death_rate, upper_recovery_rate,
-          deaths,population$India, plot_data)
+  analyse(
+    init, infected, death_rate, recovery_rate, deaths, population$India,
+    plot_data
+  )
+  analyse(
+    init, infected, death_rate, upper_recovery_rate,
+    deaths, population$India, plot_data
+  )
   return(0)
 }
 
@@ -89,7 +95,7 @@ analyse <- function(init,
   print(opt$message)
 
   opt_par <- setNames(opt$par, c("beta", "gamma"))
-  opt_par[["N"]] <- popn 
+  opt_par[["N"]] <- popn
 
   # time in days
   t <- 1:365
@@ -119,9 +125,11 @@ analyse <- function(init,
     lwd = 2, col = col, inset = 0.05
   )
 
-  suppressWarnings(matplot(fit$time, fit[, 2:4], type = "l", xlab = "Day", ylab
-                           = "Number of
-          subjects", lwd = 2, lty = 1, col = col, log = "y"))
+  suppressWarnings(matplot(fit$time, fit[, 2:4],
+    type = "l", xlab = "Day", ylab
+    = "Number of
+          subjects", lwd = 2, lty = 1, col = col, log = "y"
+  ))
 
   points(seq(length(infected)), infected)
   legend("bottomright", c("Susceptibles", "Infecteds", "Recovereds"),
@@ -149,8 +157,10 @@ analyse <- function(init,
 
 # world population 7.7 billion
 # india population 1.37 billion
-population <- list(World = 7700000000,
-India = 1370000000)
+population <- list(
+  World = 7700000000,
+  India = 1370000000
+)
 
 if (identical(environment(), globalenv())) {
   quit(status = main(commandArgs(trailingOnly = TRUE)))
