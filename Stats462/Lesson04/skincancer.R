ded1e81e3461eca008c66516fc40c5ba0b5cee7f
fernal73 <linus.fernandes@gmail.com>
2020-03-18 14:11:36 +0530

    Draw confidence and prediction intervals for prediction

---
+++ Stats462/Lesson04/skincancer.R
@@ -0,0 +1,53 @@
+#!/usr/bin/env Rscript
+library(skimr)
+
+main <- function(argv) {
+  data <- read.table("../Data/skincancer.txt",
+    header = TRUE
+  )
+  print(head(data))
+  print(skimr::skim(data))
+  reg <- lm(Mort ~ Lat, data = data)
+  print(reg)
+  print(summary(reg))
+  newdata <- data.frame(Lat = c(150))
+  print("Confidence Interval")
+  print(predict(
+                reg,
+                newdata,
+                interval = "confidence"))
+  print("Prediction Interval")
+  print(predict(
+                reg,
+                newdata,
+                interval = "prediction"))
+  return(0)
+}
+
+plot_skin_cancer <- function(data, reg) {
+  par(mar = c(4, 7, 4, 1))
+  plot(data$Lat, data$Mort,
+    main = "Skin cancer mortality versus state latitude",
+    xlab = "Latitude (at centre of state)", ylab = "Mortality (Deaths per 10
+     million)",
+    pch = 19, frame = FALSE
+  )
+  abline(reg, col = "blue")
+  x0 <- mean(data$Lat)
+  y0 <- mean(data$Mort)
+  x1 <- x0 + 5
+  y1 <- y0 + 40
+  arrows(x1, y1, x0, y0,
+    angle = 30, code = 2, col = "black", lwd = 4
+  )
+  coefs <- reg$coefficients
+  text(x1, y1 + 10, paste(
+    coefs["(Intercept)"],
+    "\n",
+    coefs["Lat"], "Lat"
+  ))
+}
+
+if (identical(environment(), globalenv())) {
+  quit(status = main(commandArgs(trailingOnly = TRUE)))
+}
