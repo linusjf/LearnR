c5f12a1e5a112cd46b99bb2ccb8c562167e35b2d
fernal73 <linus.fernandes@gmail.com>
2020-03-19 20:58:46 +0530

    Add linear regression

--- Stats462/Lesson05/iqsize.R
+++ Stats462/Lesson05/iqsize.R
@@ -8,6 +8,9 @@ main <- function(argv) {
   )
   print(head(data))
   print(skimr::skim(data))
+  reg <- lm(PIQ ~ Brain + Height + Weight, data = data)
+  print(reg)
+  print(summary(reg))
   # nolint start
   scatterplot_matrix(data, "IQ Scatterplot Matrix")
   # nolint end
