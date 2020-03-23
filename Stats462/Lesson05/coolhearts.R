0135a2ca501aa9d28e88e02470dd0b3015d8ca26
fernal73 <linus.fernandes@gmail.com>
2020-03-23 20:14:54 +0530

    Fix coolhearts to draw lines for each group

--- Stats462/Lesson05/coolhearts.R
+++ Stats462/Lesson05/coolhearts.R
@@ -19,9 +19,21 @@ main <- function(argv) {
 # together with variable you want to plot against
 predicted_df <- data.frame(Infarc = predict(reg, data),
                           Area = data$Area, Group = data$Group)
+labels <- c("Early cooling",
+"Late cooling",
+"No cooling")
 
-data %>% ggplot(aes(x = Area, y = Infarc, color = factor(Group))) +
-  geom_point()
+data %>%
+  ggplot(aes(x = Area, y = Infarc, color = factor(Group, labels = labels))) +
+  ggtitle("Scatter plot of infarction versus area") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  xlab("Size of area at risk (grams)") +
+  ylab("Size of Infarcted area (grams)") +
+  labs(color = "Group") +
+  geom_point(size = 3) +
+  geom_line(color = "red", data = subset(predicted_df, Group == 1)) +
+  geom_line(color = "green", data = subset(predicted_df, Group == 2)) +
+  geom_line(color = "blue", data = subset(predicted_df, Group == 3))
 ggplot2::ggsave("coolhearts.pdf")
   return(0)
 }
