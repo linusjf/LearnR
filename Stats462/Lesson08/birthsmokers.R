407f669c8b363188c7842c00bc57180283e73f7a
fernal73 <linus.fernandes@gmail.com>
2020-04-04 13:57:24 +0530

    Add plot3D package

--- Stats462/Lesson08/birthsmokers.R
+++ Stats462/Lesson08/birthsmokers.R
@@ -1,5 +1,6 @@
 #!/usr/bin/env Rscript
 library(skimr)
+library(scatterplot3d)
 suppressPackageStartupMessages(
                                library(dplyr))
 library(magrittr)
@@ -14,7 +15,16 @@ main <- function(argv) {
   scatterplot_matrix(data, "Birth Smokers Scatterplot Matrix")
   data %<>%
     mutate(bin_smokers = ifelse(data$Smoke == "yes", 1, 0))
+  data$Smoke <- NULL
   print(head(data))
+  # 3D scatter plot
+  with(data, {
+  s3d <- scatterplot3d(data,
+    type = "p", color = "blue",
+    angle = 55, pch = 16
+  )
+  }
+  )
   return(0)
 }
 
