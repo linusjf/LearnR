170f8232a192df61bf44156f74619d789495fab0
fernal73 <linus.fernandes@gmail.com>
2020-04-04 15:44:39 +0530

    Add fitted lines for smokers and non smokers

--- Stats462/Lesson08/birthsmokers.R
+++ Stats462/Lesson08/birthsmokers.R
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 library(skimr)
 library(scatterplot3d)
+library(plot3D)
 suppressPackageStartupMessages(
                                library(dplyr))
 library(magrittr)
@@ -14,16 +15,38 @@ main <- function(argv) {
   print(skimr::skim(data))
   scatterplot_matrix(data, "Birth Smokers Scatterplot Matrix")
   data %<>%
-    mutate(bin_smokers = ifelse(data$Smoke == "yes", 1, 0))
-  data$Smoke <- NULL
+    mutate(Smoke = ifelse(data$Smoke == "yes", 1, 0)) %>%
+    select(Wgt, Gest, Smoke)
   print(head(data))
   # 3D scatter plot
-  with(data, {
   s3d <- scatterplot3d(data,
     type = "p", color = "blue",
     angle = 55, pch = 16
   )
-  }
+  plot3D::scatter3D(data$Wgt,
+                    data$Gest,
+                    data$Smoke,
+  xlab = "Wgt", ylab = "Gest",
+  zlab = "Smoke",
+  zlim = c(0, 1),
+  type = "p", bty = "f", colkey = FALSE,
+  ticktype = "detailed")
+  s <- subset(data, data$Smoke == 1)
+  lm <- lm(Wgt ~ Gest + Smoke, s)
+  plot3D::lines3D(lm$fitted.values,
+                    s$Gest,
+                    s$Smoke,
+  zlim = c(0, 1),
+  type = "l",
+  add = TRUE, colkey = FALSE
+  )
+  ns <- subset(data, data$Smoke == 0)
+  lm <- lm(Wgt ~ Gest, ns)
+  plot3D::lines3D(lm$fitted.values,
+                    ns$Gest,
+                    ns$Smoke,
+  zlim = c(0, 1),
+  type = "l", add = TRUE, colkey = FALSE
   )
   return(0)
 }
