3cb26c9b7d2416febd7c8df94e32fdc10e0d10b2
fernal73 <linus.fernandes@gmail.com>
2020-04-04 16:57:36 +0530

    Add lines to scatterplot3d

--- Stats462/Lesson08/birthsmokers.R
+++ Stats462/Lesson08/birthsmokers.R
@@ -18,11 +18,19 @@ main <- function(argv) {
     mutate(Smoke = ifelse(data$Smoke == "yes", 1, 0)) %>%
     select(Wgt, Gest, Smoke)
   print(head(data))
+  s <- subset(data, data$Smoke == 1)
+  lm <- lm(Wgt ~ Gest + Smoke, s)
   # 3D scatter plot
   s3d <- scatterplot3d(data,
     type = "p", color = "blue",
     angle = 55, pch = 16
   )
+  s3d$points3d(lm$fitted.values,
+  s$Gest, s$Smoke, type = "l")
+  ns <- subset(data, data$Smoke == 0)
+  lm <- lm(Wgt ~ Gest, ns)
+  s3d$points3d(lm$fitted.values,
+  ns$Gest, ns$Smoke, type = "l")
   plot3D::scatter3D(data$Wgt,
                     data$Gest,
                     data$Smoke,
