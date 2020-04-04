d21180157539059ba107a9a0869bb647bb38d4f8
fernal73 <linus.fernandes@gmail.com>
2020-04-04 21:24:45 +0530

    Add birthsmokers analysis

---
+++ Stats462/Lesson08/birthsmokers_02.R
@@ -0,0 +1,17 @@
+#!/usr/bin/env Rscript
+library(skimr)
+source("../Lib/libfunc.R")
+
+main <- function(argv) {
+  data <- read.table("../Data/birthsmokers_02.txt",
+    header = TRUE, fill = TRUE
+  )
+  print(head(data))
+  print(skimr::skim(data))
+  lm <- lm(Wgt ~ Gest + Smoke, data)
+  return(0)
+}
+
+if (identical(environment(), globalenv())) {
+  quit(status = main(commandArgs(trailingOnly = TRUE)))
+}
