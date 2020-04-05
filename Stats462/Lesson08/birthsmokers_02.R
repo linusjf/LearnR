7b5c533130724ce1a04ee8273ee95e90590f69f6
fernal73 <linus.fernandes@gmail.com>
2020-04-05 06:57:03 +0530

    Add prediction

--- Stats462/Lesson08/birthsmokers_02.R
+++ Stats462/Lesson08/birthsmokers_02.R
@@ -10,6 +10,27 @@ main <- function(argv) {
   print(skimr::skim(data))
   lm <- lm(Wgt ~ Gest + Smoke, data)
   print(summary(lm))
+
+  newdata <- data.frame(Gest = c(38, 38), Smoke = c(0, 1))
+  prediction <- predict(lm, newdata, se.fit = TRUE,
+  interval = "confidence")
+  print(prediction$fit)
+
+  lm <- lm(Wgt_0 ~ Gest_0, data)
+  print(summary(lm))
+
+  newdata <- data.frame(Gest_0 = c(38), Smoke = c(0))
+  prediction <- predict(lm, newdata, se.fit = TRUE,
+  interval = "confidence")
+  print(prediction$fit)
+
+  lm <- lm(Wgt_1 ~ Gest_1, data)
+  print(summary(lm))
+
+  newdata <- data.frame(Gest_1 = c(38), Smoke = c(1))
+  prediction <- predict(lm, newdata, se.fit = TRUE,
+  interval = "confidence")
+  print(prediction$fit)
   return(0)
 }
 
