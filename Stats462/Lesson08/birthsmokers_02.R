89aa43ad6aa822bc92414b4c8d5f2346d4970ef1
fernal73 <linus.fernandes@gmail.com>
2020-04-05 08:06:42 +0530

    Display se and ci width for Gest

--- Stats462/Lesson08/birthsmokers_02.R
+++ Stats462/Lesson08/birthsmokers_02.R
@@ -8,29 +8,61 @@ main <- function(argv) {
   )
   print(head(data))
   print(skimr::skim(data))
-  lm <- lm(Wgt ~ Gest + Smoke, data)
-  print(summary(lm))
 
-  newdata <- data.frame(Gest = c(38, 38), Smoke = c(0, 1))
+  lm <- lm(Wgt ~ Gest + Smoke, data)
+  summ <- summary(lm)
+  coefficients <- summ$coefficients
+  segest <- coefficients["Gest", "Std. Error"]
+  newdata <- data.frame(Gest = c(38, 38),
+                        Smoke = c(0, 1))
   prediction <- predict(lm, newdata, se.fit = TRUE,
   interval = "confidence")
-  print(prediction$fit)
+  fit <- prediction$fit
+  width <- fit[, 3] - fit [, 2]
+  names(width) <- c("NS", "S")
 
+  summary <-
+    data.frame(
+               `Model estimated using…` =
+                 c("all 32 data points"),
+               `SE(Gest)` = c(segest),
+               `Width of CI for μY` =
+                 paste(toString(names(width)),
+                                   toString(width))
+               )
   lm <- lm(Wgt_0 ~ Gest_0, data)
-  print(summary(lm))
+  summ <- summary(lm)
+  coefficients <- summ$coefficients
+  segest <- coefficients["Gest_0", "Std. Error"]
 
   newdata <- data.frame(Gest_0 = c(38), Smoke = c(0))
   prediction <- predict(lm, newdata, se.fit = TRUE,
   interval = "confidence")
-  print(prediction$fit)
+  fit <- prediction$fit
+  width <- fit[, 3] - fit [, 2]
+  summ0 <- data.frame(`Model estimated using…` = c("16 non-smokers"),
+                        `SE(Gest)` = c(segest),
+                        `Width of CI for μY` = c(paste(width, collapse = " "))
+                        )
+
 
   lm <- lm(Wgt_1 ~ Gest_1, data)
-  print(summary(lm))
+  summ <- summary(lm)
+  coefficients <- summ$coefficients
+  segest <- coefficients["Gest_1", "Std. Error"]
 
   newdata <- data.frame(Gest_1 = c(38), Smoke = c(1))
   prediction <- predict(lm, newdata, se.fit = TRUE,
   interval = "confidence")
-  print(prediction$fit)
+  fit <- prediction$fit
+  width <- fit[, 3] - fit [, 2]
+  summ1 <- data.frame(`Model estimated using…` = c("16 smokers"),
+                        `SE(Gest)` = c(segest),
+                        `Width of CI for μY` = c(paste(width, collapse = " "))
+                        )
+  summary <- rbind(summary, summ0)
+  summary <- rbind(summary, summ1)
+  print(summary)
   return(0)
 }
 
