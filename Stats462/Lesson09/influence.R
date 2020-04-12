82feb867d4687b19d235d6e5cd45f1a38315e898
fernal73 <linus.fernandes@gmail.com>
2020-04-13 03:52:51 +0530

    Complete analysis

--- Stats462/Lesson09/influence.R
+++ Stats462/Lesson09/influence.R
@@ -45,6 +45,8 @@ source(lib_path())
 main <- function(argv) {
   noinfluence()
   outlieronly()
+  leverage()
+  all()
   return(0)
 }
 
@@ -69,6 +71,72 @@ outlieronly <- function(argv) {
   data1 <- head(data, -1)
   plot(data1$x, data1$y, xlab = "X", ylab = "Y",
   main = "Scatter plot of y versus x")
+  data2 <- tail(data, 1)
+  points(data2$x, data2$y, col = "red")
+  lm <- lm(y ~ x, data)
+  eqn <- model_equation(lm, digits = 4, trim = TRUE)
+  abline(lm)
+  print(summary(lm))
+  lm <- lm(y ~ x, data1)
+  eqn2 <- model_equation(lm, digits = 4, trim = TRUE)
+  abline(lm, col = "red")
+  print(summary(lm))
+  labels <- c(eqn, eqn2)
+  cols <- c("black", "red")
+  legend("bottomright",
+    col = cols,
+    legend = labels, lty = 1:2,
+    text.col = cols
+  )
+}
+
+leverage <- function(argv) {
+  data <- read.table(influence3.txt(),
+    header = TRUE
+  )
+  print(head(data))
+  print(skimr::skim(data))
+  plot(data$x, data$y, xlab = "X", ylab = "Y",
+  main = "Scatter plot of y versus x")
+  data1 <- head(data, -1)
+  plot(data1$x, data1$y, xlab = "X", ylab = "Y",
+  main = "Scatter plot of y versus x",
+  xlim = c(min(data$x), max(data$x)),
+  ylim = c(min(data$y), max(data$y))
+  )
+  data2 <- tail(data, 1)
+  points(data2$x, data2$y, col = "red")
+  lm <- lm(y ~ x, data)
+  eqn <- model_equation(lm, digits = 4, trim = TRUE)
+  abline(lm)
+  print(summary(lm))
+  lm <- lm(y ~ x, data1)
+  eqn2 <- model_equation(lm, digits = 4, trim = TRUE)
+  abline(lm, col = "red")
+  print(summary(lm))
+  labels <- c(eqn, eqn2)
+  cols <- c("black", "red")
+  legend("bottomright",
+    col = cols,
+    legend = labels, lty = 1:2,
+    text.col = cols
+  )
+}
+
+all <- function(argv) {
+  data <- read.table(influence4.txt(),
+    header = TRUE
+  )
+  print(head(data))
+  print(skimr::skim(data))
+  plot(data$x, data$y, xlab = "X", ylab = "Y",
+  main = "Scatter plot of y versus x")
+  data1 <- head(data, -1)
+  plot(data1$x, data1$y, xlab = "X", ylab = "Y",
+  main = "Scatter plot of y versus x",
+  xlim = c(min(data$x), max(data$x)),
+  ylim = c(min(data$y), max(data$y))
+  )
   data2 <- tail(data, 1)
   points(data2$x, data2$y, col = "red")
   lm <- lm(y ~ x, data)
