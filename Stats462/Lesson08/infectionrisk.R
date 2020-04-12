e3cc8c417af9c1fbca3bcfa6736d83ba917e8c2c
fernal73 <linus.fernandes@gmail.com>
2020-04-12 11:55:04 +0530

    Add infection risk analysis

--- Stats462/Lesson08/infectionrisk.R
+++ Stats462/Lesson08/infectionrisk.R
@@ -25,25 +25,44 @@ main <- function(argv) {
   data <- read.table(infectionrisk.txt(),
     header = TRUE
   )
-  print(head(data))
-  print(skimr::skim(data))
-
   data %<>%
     mutate(I2 = ifelse(Region == 2, 1, 0)) %>%
     mutate(I3 = ifelse(Region == 3, 1, 0)) %>%
     mutate(I4 = ifelse(Region == 4, 1, 0))
-  
-  simple <- fit_simple(data)
+
+  print(head(data))
+  print(skimr::skim(data))
+
+  full <- fit_full(data)
+  print(summary(full))
+
+  reduced <- fit_reduced(data)
+
+  anova <- anova(reduced, full)
+
+  print(anova)
+
+  westonly <- fit_westonly(data)
+
+  anova <- anova(westonly, full)
+
+  print(anova)
 
   return(0)
 }
 
-fit_simple <- function(data) {
+fit_full <- function(data) {
   lm <- lm(InfctRsk ~ Stay + Xray + I2 + I3 + I4, data)
-  summ <- summary(lm)
-  anova <- anova(lm)
-  print(summ)
-  print(anova)
+  return(lm)
+}
+
+fit_reduced <- function(data) {
+  lm <- lm(InfctRsk ~ Stay + Xray, data)
+  return(lm)
+}
+
+fit_westonly <- function(data) {
+  lm <- lm(InfctRsk ~ Stay + Xray + I4, data)
   return(lm)
 }
 
