46431fb88dd24445cb478fed51eddb450b132376
fernal73 <linus.fernandes@gmail.com>
2020-04-12 20:39:19 +0530

    Add influence example

---
+++ Stats462/Lesson09/influence.R
@@ -0,0 +1,82 @@
+#!/usr/bin/env Rscript
+influence1.txt <- function() {
+  library(rprojroot)
+  paste0(
+    find_root(has_file(".Rprofile")),
+    "/Stats462/Data/influence1.txt"
+  )
+}
+
+influence2.txt <- function() {
+  library(rprojroot)
+  paste0(
+    find_root(has_file(".Rprofile")),
+    "/Stats462/Data/influence2.txt"
+  )
+}
+
+influence3.txt <- function() {
+  library(rprojroot)
+  paste0(
+    find_root(has_file(".Rprofile")),
+    "/Stats462/Data/influence3.txt"
+  )
+}
+
+influence4.txt <- function() {
+  library(rprojroot)
+  paste0(
+    find_root(has_file(".Rprofile")),
+    "/Stats462/Data/influence4.txt"
+  )
+}
+
+lib_path <- function() {
+  library(rprojroot)
+  paste0(
+    find_root(has_file(".Rprofile")),
+    "/Stats462/Lib/libfunc.R"
+  )
+}
+
+library(skimr)
+source(lib_path())
+
+main <- function(argv) {
+  noinfluence()
+  outlieronly()
+  return(0)
+}
+
+noinfluence <- function(argv) {
+  data <- read.table(influence1.txt(),
+    header = TRUE
+  )
+  print(head(data))
+  print(skimr::skim(data))
+  plot(data$x, data$y, xlab = "X", ylab = "Y",
+  main = "Scatter plot of y versus x")
+}
+
+outlieronly <- function(argv) {
+  data <- read.table(influence2.txt(),
+    header = TRUE
+  )
+  print(head(data))
+  print(skimr::skim(data))
+  plot(data$x, data$y, xlab = "X", ylab = "Y",
+  main = "Scatter plot of y versus x")
+  data1 <- head(data, -1)
+  plot(data1$x, data1$y, xlab = "X", ylab = "Y",
+  main = "Scatter plot of y versus x")
+  data2 <- tail(data, 1)
+  points(data2$x, data2$y, col = "red")
+  lm <- lm(y ~ x, data)
+  abline(lm)
+  lm <- lm(y ~ x, data1)
+  abline(lm, col = "red")
+}
+
+if (identical(environment(), globalenv())) {
+  quit(status = main(commandArgs(trailingOnly = TRUE)))
+}
