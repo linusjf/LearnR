893228dcddaa07feff5b371e6cccbfca653d347f
fernal73 <linus.fernandes@gmail.com>
2020-04-13 09:56:54 +0530

    Refactor code

--- Stats462/Lesson09/extreme.R
+++ Stats462/Lesson09/extreme.R
@@ -36,35 +36,16 @@ source(lib_path())
 suppressPackageStartupMessages(library(VGAM))
 
 main <- function(argv) {
-  influence2()
-  influence3()
-  influence4()
+
+  lapply(c(influence2.txt(),
+           influence3.txt(),
+           influence4.txt()),
+  influence)
   return(0)
 }
 
-influence2 <- function(argv) {
-  data <- read.table(influence2.txt(),
-    header = TRUE
-  )
-  print(head(data))
-  print(skimr::skim(data))
-  data1 <- head(data, -1)
-  plot(data1$x, data1$y, xlab = "X", ylab = "Y",
-  main = "Scatter plot of y versus x")
-  data2 <- tail(data, 1)
-  points(data2$x, data2$y, col = "red")
-  lm <- lm(y ~ x, data)
-  hat <- hatvalues(lm)
-  plot(data$x, hat, xlab = "X", ylab = "Hat values",
-  main = "Scatter plot of hat values versus x")
-  abline(v = mean(data$x))
-  print("Sum hat values: ")
-  print(sum(hat))
-  boxplot(hat)
-}
-
-influence3 <- function(argv) {
-  data <- read.table(influence3.txt(),
+influence <- function(path) {
+  data <- read.table(path,
     header = TRUE
   )
   print(head(data))
@@ -72,6 +53,7 @@ influence3 <- function(argv) {
   data1 <- head(data, -1)
   plot(data1$x, data1$y, xlab = "X", ylab = "Y",
   main = "Scatter plot of y versus x",
+  sub = path,
   xlim = c(min(data$x), max(data$x)),
   ylim = c(min(data$y), max(data$y))
   )
@@ -79,36 +61,24 @@ influence3 <- function(argv) {
   points(data2$x, data2$y, col = "red")
   lm <- lm(y ~ x, data)
   hat <- hatvalues(lm)
-  plot(data$x, hat, xlab = "X", ylab = "Hat values",
-  main = "Scatter plot of hat values versus x")
-  abline(v = mean(data$x))
-  print("Sum hat values: ")
-  print(sum(hat))
-  boxplot(hat)
-}
-
-influence4 <- function(argv) {
-  data <- read.table(influence4.txt(),
-    header = TRUE
-  )
-  print(head(data))
-  print(skimr::skim(data))
-  data1 <- head(data, -1)
-  plot(data1$x, data1$y, xlab = "X", ylab = "Y",
-  main = "Scatter plot of y versus x",
+  hat1 <- head(hat, -1)
+  plot(data1$x, hat1, xlab = "X", ylab = "Hat values",
+  main = "Scatter plot of hat values versus x",
   xlim = c(min(data$x), max(data$x)),
-  ylim = c(min(data$y), max(data$y))
+  ylim = c(min(hat), max(hat))
   )
-  data2 <- tail(data, 1)
-  points(data2$x, data2$y, col = "red")
-  lm <- lm(y ~ x, data)
-  hat <- hatvalues(lm)
-  plot(data$x, hat, xlab = "X", ylab = "Hat values",
-  main = "Scatter plot of hat values versus x")
+  hat2 <- tail(hat, 1)
+  points(data2$x, hat2, col = "red")
   abline(v = mean(data$x))
   print("Sum hat values: ")
   print(sum(hat))
-  boxplot(hat)
+  boxplot(hat,
+  main = path,
+  xlab = "Hat values",
+  col = "orange",
+  border = "brown",
+  horizontal = TRUE,
+  notch = FALSE)
 }
 
 if (identical(environment(), globalenv())) {
