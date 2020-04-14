928c5f200f99e3dec3474ebf75e6e723f440f76a
fernal73 <linus.fernandes@gmail.com>
2020-04-14 20:22:41 +0530

    Plot dffits

--- Stats462/Lesson09/identify.R
+++ Stats462/Lesson09/identify.R
@@ -35,6 +35,7 @@ library(skimr)
 source(lib_path())
 suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(magrittr))
+suppressPackageStartupMessages(library(olsrr))
 
 main <- function(argv) {
   data <- c(influence2.txt(),
@@ -42,21 +43,41 @@ main <- function(argv) {
             influence4.txt()
   )
 
-  lapply(data, dffits)
+  lapply(data, dffits_plot)
   return(0)
 }
 
-dffits <- function(path) {
+dffits_plot <- function(path) {
   data <- read.table(path,
     header = TRUE
   )
-  lm <- lm(y ~ x, data)
+  model <- lm(y ~ x, data)
+  print(model$rank)
+  data %<>%
+    mutate(dffits = dffits(model, infl = lm.influence(model)))
   print(head(data))
   print(skimr::skim(data))
   plot(data$x, data$y, xlab = "X", ylab = "Y",
   main = "Scatter plot of y versus x")
   data2 <- tail(data, 1)
   points(data2$x, data2$y, col = "red")
+  n <- nrow(data)
+  k <- length(model$coefficients) - 1
+  cv <- 2 * sqrt((k + 2) / (n - k - 2))
+  miny <- min(data$dffits, -cv)
+  maxy <- max(data$dffits, cv)
+  if (abs(miny) < maxy)
+    miny <- -maxy
+  if (abs(miny) > maxy)
+    maxy <- -miny
+  plot(data$x, data$dffits, xlab = "X", ylab = "DFFITS",
+  main = "Standardized dffits",
+  ylim = c(miny, maxy),
+  sub = paste0("Critical value = (+/-)", round(cv, 4))
+  )
+  # critical horizontal value lines
+  abline(h = cv, lty = 2)
+  abline(h = -cv, lty = 2)
 }
 
 if (identical(environment(), globalenv())) {
