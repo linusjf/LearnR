8590f4efc524aa1c9d0672fe39fe324f925ecc56
fernal73 <linus.fernandes@gmail.com>
2020-04-17 15:30:15 +0530

    add log conversion

--- Stats462/Lesson10/realestate.R
+++ Stats462/Lesson10/realestate.R
@@ -17,6 +17,8 @@ lib_path <- function() {
 
 library(skimr)
 source(lib_path())
+suppressPackageStartupMessages(library(magrittr))
+suppressPackageStartupMessages(library(dplyr))
 
 main <- function(argv) {
   data <- read.table(realestate.txt(),
@@ -32,6 +34,22 @@ main <- function(argv) {
   print(model_coeffs(model.1))
   eqn.1 <- model_equation(model.1, digits = 4)
   print(eqn.1)
+
+  detach(data)
+
+  data %<>%
+    mutate(lnSalePrice = log(SalePrice)) %>%
+    mutate(lnSqFeet = log(SqFeet)) %>%
+    mutate(lnLot = log(Lot))
+
+  attach(data)
+  model.log <- lm(lnSalePrice ~ lnSqFeet + lnLot, data)
+  print(anova(model.log))
+  print(model_fit_stats(model.log))
+  print(model_coeffs(model.log))
+  eqn.log <- model_equation(model.log, digits = 4)
+  print(eqn.log)
+
   detach(data)
 
   return(0)
