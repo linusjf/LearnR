162ff6d44cb29dcc606522bf30f9d4b73e5fc634
fernal73 <linus.fernandes@gmail.com>
2020-04-17 21:45:33 +0530

    Add time series example

---
+++ Stats462/Lesson10/google_stock.R
@@ -0,0 +1,46 @@
+#!/usr/bin/env Rscript
+# nolint start
+google_stock.txt <- function() {
+  library(rprojroot)
+  paste0(
+    find_root(has_file(".Rprofile")),
+    "/Stats462/Data/google_stock.txt"
+  )
+}
+# nolint end
+
+lib_path <- function() {
+  library(rprojroot)
+  paste0(
+    find_root(has_file(".Rprofile")),
+    "/Stats462/Lib/libfunc.R"
+  )
+}
+
+library(skimr)
+source(lib_path())
+
+main <- function(argv) {
+  pdf(paper = "USr")
+  data <- read.table(google_stock.txt(),
+    header = TRUE, as.is = TRUE
+  )
+  print(str(data$date))
+  print(head(data))
+  data$date <- as.POSIXct(data$date, format = "%m/%d/%Y")
+  print(skimr::skim(data))
+
+  attach(data)
+  plot(
+    x = date, y = price, col = "red",
+    pch = 15, type = "b",
+    main = "Time series plot of price versus date",
+    xlab = "Date", ylab = "Price"
+  )
+  detach(data)
+  return(0)
+}
+
+if (identical(environment(), globalenv())) {
+  quit(status = main(commandArgs(trailingOnly = TRUE)))
+}
