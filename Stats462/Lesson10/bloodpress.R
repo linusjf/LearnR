e4006b2b1087c8f171b57452c764829b7dd44179
fernal73 <linus.fernandes@gmail.com>
2020-04-22 09:53:01 +0530

    Start bloodpress example

---
+++ Stats462/Lesson10/bloodpress.R
@@ -0,0 +1,34 @@
+#!/usr/bin/env Rscript
+bloodpress.txt <- function() {
+  library(rprojroot)
+  paste0(
+    find_root(has_file(".Rprofile")),
+    "/Stats462/Data/bloodpress.txt"
+  )
+}
+
+lib_path <- function() {
+  library(rprojroot)
+  paste0(
+    find_root(has_file(".Rprofile")),
+    "/Stats462/Lib/libfunc.R"
+  )
+}
+
+library(skimr)
+source(lib_path())
+
+main <- function(argv) {
+  cairo_pdf(onefile = TRUE)
+  data <- read.table(bloodpress.txt(),
+    header = TRUE, as.is = TRUE
+  )
+  print(head(data))
+  print(skimr::skim(data))
+
+  return(0)
+}
+
+if (identical(environment(), globalenv())) {
+  quit(status = main(commandArgs(trailingOnly = TRUE)))
+}
