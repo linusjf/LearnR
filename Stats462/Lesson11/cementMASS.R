3e594a9732c7eadc6eeaf253b8156a7d382cf0e1
fernal73 <linus.fernandes@gmail.com>
2020-04-27 15:16:54 +0530

    Cement example with stepAIC from MASS package

---
+++ Stats462/Lesson11/cementMASS.R
@@ -0,0 +1,47 @@
+#!/usr/bin/env Rscript
+cement.txt <- function() {
+  library(rprojroot)
+  paste0(
+    find_root(has_file(".Rprofile")),
+    "/Stats462/Data/cement.txt"
+  )
+}
+
+lib_path <- function() {
+  library(rprojroot)
+  paste0(
+    find_root(has_file(".Rprofile")),
+    "/Stats462/Lib/libfunc.R"
+  )
+}
+
+library(skimr)
+source(lib_path())
+suppressPackageStartupMessages(library(PerformanceAnalytics))
+suppressPackageStartupMessages(library(MASS))
+
+main <- function(argv) {
+  cairo_pdf(onefile = TRUE)
+  data <- read.table(cement.txt(),
+    header = TRUE, as.is = TRUE
+  )
+  print(head(data))
+  print(skimr::skim(data))
+
+  chart.Correlation(data,
+    histogram = TRUE,
+    pch = 15
+  )
+
+  model <- lm(y ~ ., data)
+  final <-
+    stepAIC(model,
+         trace = 10, k = log(nrow(data)),
+    direction = "both")
+  print(final)
+  return(0)
+}
+
+if (identical(environment(), globalenv())) {
+  quit(status = main(commandArgs(trailingOnly = TRUE)))
+}
