052a646a162d3c6aa51672de6ec7beaf1c3ff2de
fernal73 <linus.fernandes@gmail.com>
2020-04-29 07:01:34 +0530

    Add new subsets example

---
+++ Stats462/Lesson11/cementsubsets.R
@@ -0,0 +1,44 @@
+#!/usr/bin/env Rscript
+cement.txt <- function() {
+  library(rprojroot)
+  paste0(
+    find_root(has_file(".Rprofile")),
+    "/Stats462/Data/cement.txt"
+  )
+}
+
+lib_path <- function() {
+  library(rprojroot)
+  paste0(
+    find_root(has_file(".Rprofile")),
+    "/Stats462/Lib/libfunc.R"
+  )
+}
+
+library(skimr)
+source(lib_path())
+suppressPackageStartupMessages(library(PerformanceAnalytics))
+suppressPackageStartupMessages(library(olsrr))
+
+main <- function(argv) {
+  cement <- read.table(cement.txt(),
+    header = TRUE, as.is = TRUE
+  )
+  print(head(cement))
+  print(skimr::skim(cement))
+
+  chart.Correlation(cement,
+    histogram = TRUE,
+    pch = 15
+  )
+  model <- lm(y ~ ., data = cement)
+  all <- ols_step_all_possible(model)
+  print(all)
+  plot.obj <- plot(all, print_plot = FALSE)
+  lapply(plot.obj, print)
+  return(0)
+}
+
+if (identical(environment(), globalenv())) {
+  quit(status = main(commandArgs(trailingOnly = TRUE)))
+}
