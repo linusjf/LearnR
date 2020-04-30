d8a74faa0c61d1491ac454e91f1d64fd44e3e8a4
fernal73 <linus.fernandes@gmail.com>
2020-04-30 13:36:31 +0530

    Add cp statistic method

--- Stats462/Lesson11/cementsubsets.R
+++ Stats462/Lesson11/cementsubsets.R
@@ -39,14 +39,19 @@ main <- function(argv) {
   print(str(best))
   best_rsq <- best_model_rsquare(best, model)
   best_adjr <- best_model_adjrsquare(best, model)
+  best_cp <- best_model_cp(best, model, "mincp")
+  best_cp2 <- best_model_cp(best, model, "relcp")
   print(best_rsq)
   print(best_adjr)
+  print(best_cp)
+  print(best_cp2)
   return(0)
 }
 
 best_model_rsquare <- function(models, model, rsqinc = 0.05) {
-  if (!inherits(models, "ols_step_best_subset"))
+  if (!inherits(models, "ols_step_best_subset")) {
     stop("Class has to be ols_step_best_subset")
+  }
   models %<>%
     mutate(rsq.inc = ((rsquare / lag(rsquare) - 1)))
   models %<>%
@@ -60,14 +65,42 @@ best_model_rsquare <- function(models, model, rsqinc = 0.05) {
 }
 
 best_model_adjrsquare <- function(models, model) {
-  if (!inherits(models, "ols_step_best_subset"))
+  if (!inherits(models, "ols_step_best_subset")) {
     stop("Class has to be ols_step_best_subset")
+  }
   models %<>%
     filter(adjr == max(adjr))
   predictors <- models$predictors
   rhs <- str_replace_all(predictors, " ", "+")
   formula <- update(formula(model), paste0(". ~ ", rhs))
   data <- model$model
+  return(lm(formula, data))
+}
+
+best_model_cp <- function(models, model, criteria = "mincp") {
+  if (!inherits(models, "ols_step_best_subset")) {
+    stop("Class has to be ols_step_best_subset")
+  }
+  data <- model$model
+  cols <- ncol(data)
+  models %<>%
+    mutate(p = n + 1) %>%
+    mutate(cpratio = cp / p) %>%
+    filter(p != cols)
+  if (criteria == "mincp") {
+    models %<>%
+      filter(cp == min(cp))
+  } else if (criteria == "relcp") {
+    models %<>%
+      filter(cpratio == min(cpratio))
+  } else {
+    stop("Criteria should be mincp or relcp")
+  }
+  models %<>%
+    filter(n == min(n))
+  predictors <- models$predictors
+  rhs <- str_replace_all(predictors, " ", "+")
+  formula <- update(formula(model), paste0(". ~ ", rhs))
   return(lm(formula, data))
 }
 
