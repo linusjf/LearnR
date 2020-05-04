3bbc0ddfd5e563b4ec33b110db47beac84042afb
fernal73 <linus.fernandes@gmail.com>
2020-05-04 12:54:46 +0530

    Add example Physical. Add info messages

--- Stats462/Lesson11/physicalsubsets.R
+++ Stats462/Lesson11/physicalsubsets.R
@@ -1,9 +1,9 @@
 #!/usr/bin/env Rscript
-peru.txt <- do.call(function() {
+Physical.txt <- do.call(function() {
   library(rprojroot)
   paste0(
     find_root(has_file(".Rprofile")),
-    "/Stats462/Data/peru.txt"
+    "/Stats462/Data/Physical.txt"
   )
 }, list())
 
@@ -30,13 +30,13 @@ suppressPackageStartupMessages(library(PerformanceAnalytics))
 suppressPackageStartupMessages(library(nortest))
 
 main <- function(argv) {
-  data <- read.table(peru.txt,
+  data <- read.table(Physical.txt,
     header = TRUE, as.is = TRUE
   )
   data %<>%
-    mutate(fraclife = Years / Age) %>%
-    select(Age, Years, fraclife, Weight, Height, Chin,
-    Forearm, Pulse, Systol)
+    mutate(Gender = ifelse(data$Sex == "Male",
+                           0, 1)) %>%
+    select(Height, LeftArm, LeftFoot, LeftHand, HeadCirc, nose, Gender)
   print(head(data))
   print(skimr::skim(data))
 
@@ -44,7 +44,7 @@ main <- function(argv) {
     histogram = TRUE,
     pch = 15
   )
-  model <- lm(Systol ~ ., data = data)
+  model <- lm(Height ~ ., data = data)
   all <- ols_step_all_possible(model)
   print(str(all))
   lmset <- fill_models(all, model)
@@ -56,49 +56,30 @@ main <- function(argv) {
   p <- 2 * pt(-abs(t), df = N - k - 1)
   print("p-value for Schwarz Bayesian equivalent")
   print(p)
-  best_p <- step_wise_regression(data, "Systol")
-  best_p2 <- step_wise_regression(data, "Systol",
+  best_p <- step_wise_regression(data, "Height")
+  best_p2 <- step_wise_regression(data, "Height",
     alpha_entry = p, alpha_removal = p
   )
-  best_rsq <- best_model_rsquare(all, model)
-  best_adjr <- best_model_adjrsquare(all, model)
-  best_cp <- best_model_cp(all, model, "mincp")
-  best_cp2 <- best_model_cp(all, model, "relcp")
   print("Best p model(s)")
   print(best_p)
   print(best_p2)
-  print("Best rsquared model(s)")
-  print(best_rsq)
-  print("Best adj rsquared model(s)")
-  print(best_adjr)
-  print("Best cp model(s)")
-  print(best_cp)
-  print(best_cp2)
   best_aic <- best_model_aic(all, model)
   best_sbc <- best_model_sbc(all, model)
   print("Best information model(s)")
   print(best_aic)
   print(best_sbc)
-  print("Computed cp(s)")
-  print(compute_cp(model, best_cp))
-  print(compute_cp(model, best_cp2))
   best <- unique_models(list(
     best_p,
     best_p2,
-    best_rsq,
-    best_adjr,
-    best_cp,
-    best_cp2,
     best_aic,
     best_sbc
   ))
-  print("Best subset using p, rsquare, adjr ,cp, aic, sbc")
+  print("Best subset using p, aic, sbc")
   print(best)
   lapply(best, checkfit)
   print("Best vif model(s)")
   models <- best_vif(best)
   print(models)
-
   lapply(models, checkfit)
   return(0)
 }
