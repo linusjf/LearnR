88e7ba9d01534364accac1d967ab34188b108179
fernal73 <linus.fernandes@gmail.com>
2021-05-05 16:42:22 +0530

    plot bar chart to pdf and png

---
+++ RSuccinctly/plots.R
@@ -0,0 +1,41 @@
+#!/usr/bin/env Rscript
+# LOAD DATA
+# Loads data sets package
+require("datasets")
+
+# DEFAULT CHART WITH PLOT()
+# Default method to plot the variable feed from chickwts
+plot(chickwts$feed)
+
+# CREATE TABLE
+feeds <- table(chickwts$feed) # Create a table of feed, place in “feeds”
+# See contents of object “feeds”
+feeds
+# Identical to plot(chickwts$feed) but with new object
+barplot(feeds)
+
+# USE BARPLOT() AND PAR() FOR PARAMETERS
+# Stores current graphical parameters.
+oldpar <- par(no.readonly = TRUE)
+par(oma = c(1, 4, 1, 1)) # Sets outside margins: bottom, left, top, right.
+par(mar = c(4, 5, 2, 1)) # Sets plot margins.
+barplot(feeds[order(feeds)], # Orders the bars by descending values.
+  horiz = TRUE, # Makes the bars horizontal.
+  las = 1, # las gives orientation of axis labels.
+  col = c(
+    "beige", "blanchedalmond", "bisque1", "bisque2",
+    "bisque3", "bisque4"
+  ), # Vector of colors for bars.
+  border = NA, # No borders on bars.
+  # Add main title and label for x-axis.
+  main = "Frequencies of Different Feeds in chickwts Data set",
+  xlab = "Number of Chicks"
+)
+
+# CLEAN UP
+# Restores previous parameters (ignore errors).
+par(oldpar)
+# Unloads data sets package.
+detach("package:datasets", unload = TRUE)
+# Removes all objects from workspace.
+rm(list = ls())
---
+++ RSuccinctly/plotspdf.R
@@ -0,0 +1,38 @@
+#!/usr/bin/env Rscript 
+# LOAD DATA
+# Loads data sets package
+require("datasets") 
+# CREATE TABLE
+# Create a table of feed, place in “feeds”
+feeds <- table(chickwts$feed) 
+# See contents of object “feeds”
+print(feeds) 
+
+# CHOOSE GRAPHICS DEVICE
+# TO SAVE AS PDF
+# EITHER this device for a PDF file (scalable vector graphics)
+pdf("plots.pdf", # Give the full path and name.
+ width = 9, # Width of image in inches.
+ height = 6) # Height of image in inches.
+
+# CREATE GRAPHIC
+# Then run the command(s) for the graphic.
+oldpar <- par(no.readonly = TRUE) # Stores current graphical parameters.
+par(oma = c(1, 1, 1, 1)) # Sets outside margins: bottom, left, top, right.
+par(mar = c(4, 5, 2, 1)) # Sets plot margins.
+barplot(feeds[order(feeds)], # Order the bars by descending values.
+ horiz = TRUE, # Make the bars horizontal.
+ las = 1, # las gives orientation of axis labels.
+ col = c("beige", "blanchedalmond", "bisque1", "bisque2",
+         "bisque3", "bisque4"), # Vector of colors for bars
+ border = NA, # No borders on bars.
+ # Add main title and label for x-axis.
+ main = "Frequencies of Different Feeds\nin chickwts Data set",
+ xlab = "Number of Chicks") 
+# CLEAN UP
+# Turns off graphics device.
+invisible(dev.off()) 
+# Restores previous graphics parameters (ignore errors).
+par(oldpar) 
+# Removes all objects from workspace.
+rm(list = ls()) 
