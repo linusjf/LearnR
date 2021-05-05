c346418bb44c793fd477ad62c174bf2adc3123e5
fernal73 <linus.fernandes@gmail.com>
2021-05-05 22:42:15 +0530

    add

--- RSuccinctly/faithful.R
+++ RSuccinctly/faithful.R
@@ -9,14 +9,22 @@ table(zapsmall(abs(e60 - ne60)))
 # 0, 0.02 or 0.04
 faithful$better.eruptions <- ne60 / 60
 te <- table(ne60)
-te[te >= 4]
+print(te[te >= 4])
 # (too) many multiples of 5 !
-plot(names(te), te, type = "h", main = f.tit, xlab = "Eruption time (sec)")
+plot(names(te), te,
+  type = "h",
+  main = f.tit,
+  xlab = "Eruption time (sec)"
+)
 plot(faithful[, -3],
   main = f.tit,
   xlab = "Eruption time (min)",
   ylab = "Waiting time to next eruption (min)"
 )
-lines(lowess(faithful$eruptions, faithful$waiting, f = 2 / 3, iter = 3),
-  col = "red"
+lines(lowess(faithful$eruptions,
+  faithful$waiting,
+  f = 2 / 3,
+  iter = 3
+),
+col = "red"
 )
