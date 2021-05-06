3886e29166d6ab4a593073a3324b0aa563f130e1
fernal73 <linus.fernandes@gmail.com>
2021-05-06 10:20:28 +0530

    Add summary stats example

---
+++ RSuccinctly/summary.R
@@ -0,0 +1,22 @@
+#!/usr/bin/env Rscript
+# LOAD DATA SET
+# Load the data sets package
+require("datasets")
+require("psych")
+# Print the cars data structure to the console
+str(cars)
+# Load the data into the workspace
+data(cars)
+# Summary for one variable
+summary(cars$speed)
+# Summary for entire table (inc. categories)
+summary(cars)
+# ALTERNATIVE DESCRIPTIVES
+describe(cars)
+# CLEAN UP
+# Unloads data sets package.
+detach("package:datasets", unload = TRUE)
+# Unloads psych package.
+detach("package:psych", unload = TRUE)
+# Remove all objects from workspace.
+rm(list = ls())
