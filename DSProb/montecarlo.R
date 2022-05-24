deacfaa1aa990dc0f96112353afe6c2e47593cad
fernal73 <linus.fernandes@gmail.com>
2022-05-24 12:29:04 +0530

    feat: add DSProb sample

---
+++ DSProb/montecarlo.R
@@ -0,0 +1,16 @@
+#!/usr/bin/env Rscript
+beads <- rep(c("red", "blue"), times = c(2,3))    # create an urn with 2 red, 3 blue
+# view beads object
+print(beads)    
+# sample 1 bead at random
+sample(beads, 1)    
+
+# number of times to draw 1 bead
+B <- 10000    
+events <- replicate(B, sample(beads, 1))    # draw 1 bead, B times
+# make a table of outcome counts
+tab <- table(events)    
+# view count table
+print(tab)
+# view table
+prop.table(tab)    
