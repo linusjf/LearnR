3dfd709445c26661942c65c408f87a589eaa11f7
fernal73 <linus.fernandes@gmail.com>
2022-05-24 19:55:12 +0530

    feat: add expandgrid script

---
+++ DSProb/expandgrid.R
@@ -0,0 +1,11 @@
+#!/usr/bin/env Rscript
+# joining strings with paste
+number <- "Three"
+suit <- "Hearts"
+paste(number, suit)
+
+# joining vectors element-wise with paste
+paste(letters[1:5], as.character(1:5))
+
+# generating combinations of 2 vectors with expand.grid
+expand.grid(pants = c("blue", "black"), shirt = c("white", "grey", "plaid"))
