be2fe95b875f65c0a5a77754626a79b7709165f9
fernal73 <linus.fernandes@gmail.com>
2022-05-25 21:08:05 +0530

    feat: update birthdays scripts for prob plots

--- DSProb/birthdays.R
+++ DSProb/birthdays.R
@@ -13,4 +13,31 @@ results <- replicate(B, {
     any(duplicated(bdays))
 })
 # calculates
-mean(results)    
+mean(results)   
+
+# function to calculate probability of shared bdays across n people
+compute_prob <- function(n, B = 10000) {
+	same_day <- replicate(B, {
+    	bdays <- sample(1:365, n, replace = TRUE)
+        any(duplicated(bdays))
+    })
+    mean(same_day)
+}
+
+# function for computing exact probability of shared birthdays for any n
+exact_prob <- function(n){
+    prob_unique <- seq(365, 365-n+1)/365   # vector of fractions for mult. rule
+    # calculate prob of no shared birthdays and subtract from 1
+    1 - prod(prob_unique)    
+}
+
+n <- seq(1, 60)
+prob <- sapply(n, compute_prob)    # element-wise application of compute_prob to n
+
+# applying function element-wise to vector of n values
+eprob <- sapply(n, exact_prob)
+
+# plotting Monte Carlo results and exact probabilities on same graph
+# plot Monte Carlo results
+plot(n, prob)    
+lines(n, eprob, col = "red")
