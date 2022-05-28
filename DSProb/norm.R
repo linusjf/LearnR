7df7c3bb1a90c9ab158017aea713ae0a41f987c5
fernal73 <linus.fernandes@gmail.com>
2022-05-28 16:05:57 +0530

    feat: add normal distribition example

---
+++ DSProb/norm.R
@@ -0,0 +1,27 @@
+#!/usr/bin/env Rscript
+# define x as male heights from dslabs data
+suppressPackageStartupMessages(library(tidyverse))
+library(dslabs)
+data(heights)
+x <- heights %>% filter(sex=="Male") %>% 
+  pull(height)
+
+# generate simulated height data using normal distribution - both datasets should have n observations
+n <- length(x)
+avg <- mean(x)
+s <- sd(x)
+simulated_heights <- rnorm(n, avg, s)
+
+# plot distribution of simulated_heights
+data.frame(simulated_heights = simulated_heights) %>%
+    ggplot(aes(simulated_heights)) +
+    geom_histogram(color="black", binwidth = 2)
+
+B <- 10000
+tallest <- replicate(B, {
+    simulated_data <- rnorm(800, avg, s)    # generate 800 normally distributed random heights
+    # determine the tallest height
+    max(simulated_data)    
+})
+# proportion of 7 footers
+mean(tallest >= 7*12)    
