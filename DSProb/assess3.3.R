88a2371cde40e1add601d0d2f7d757e02851e048
fernal73 <linus.fernandes@gmail.com>
2022-06-01 09:11:44 +0000

    feat: continue on assessment 3.3

--- DSProb/assess3.3.R
+++ DSProb/assess3.3.R
@@ -2,14 +2,16 @@
 
 print("What is the probability of guessing correctly for one question?")
 p <- 0.2
-p
+q <- 1 - p
 print("What is the expected value of points for guessing on one question?")
 a <- 1
 b <- -0.25
-mu <- a * p + b * (1 -p)
+mu <- a * p + b * q
 mu
 print("What is the expected score of guessing on all 44 questions?")
 n <- 44
 n * mu
-
-
+print("What is the standard error of guessing on all 44 questions?")
+sigma <- abs(b - a) * sqrt(p * q)
+se <- sqrt(n) * sigma
+se
