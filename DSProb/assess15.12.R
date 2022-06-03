94ca05e74d68e964e549117570012f558caa235f
fernal73 <linus.fernandes@gmail.com>
2022-06-03 05:50:50 +0000

    feat: finish assessment 15.12

--- DSProb/assess15.12.R
+++ DSProb/assess15.12.R
@@ -29,5 +29,14 @@ se <- sqrt(n * p * (1 - p)) * abs(loss_per_foreclosure)
 se / 10^6
 
 print("Suppose we give out loans for $180,000. What should the interest rate be so that our expected value is 0?")
+x <- -loss_per_foreclosure * p / (1-p)
+x
+x/loan.amt
 
-print("(Harder) What should the interest rate be so that the chance of losing money is 1 in 20? In math notation, what should the interest rate be so that Pr( S < 0 ) =0.05")
+print("(Harder) What should the interest rate be so that the chance of losing money is 1 in 20? In math notation, what should the interest rate be so that Pr( S < 0 ) = 0.05")
+z <- qnorm(0.05)
+l <- loss_per_foreclosure
+z <- qnorm(0.01)
+x <- -l*( n*p - z*sqrt(n*p*(1-p)))/ ( n*(1-p) + z*sqrt(n*p*(1-p)))
+x
+x / loan.amt
