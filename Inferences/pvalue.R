6c155497b1e56ffc7e1fc4f837f761f24f44fefd
Linus Fernandes <linus.fernandes@gmail.com>
2023-05-10 16:47:32 +0530

    feat: start pvalue example

--- Inferences/pvalue.R
+++ Inferences/pvalue.R
@@ -66,3 +66,81 @@ axis(side=2, at=seq(0,nSims, nSims/4), labels=seq(0,nSims, nSims/4), las=2)
 abline(h=nSims/bars, col = "red", lty=3)
 power <- sum(p < 0.05) / 100000 # compute power
 print(power)
+
+n <- 71 # sample size per independent group
+d <- 0.33 # effect size
+se <- sqrt(2 / n) # standard error
+ncp <- (d * sqrt(n / 2)) # non-centrality parameter d
+
+pdf2_t <- function(p) { #probability density function
+  0.5 * dt(qt(p / 2, 2 * n - 2, 0), 2 * n - 2, ncp) /
+      dt(qt(p / 2, 2 * n - 2, 0), 2 * n - 2, 0) +
+    dt(qt(1 - p / 2, 2 * n - 2, 0), 2 * n - 2, ncp) /
+      dt(qt(1 - p / 2, 2 * n - 2, 0), 2 * n - 2, 0)
+}
+
+par(bg = backgroundcolor)
+plot(-10, xlab = "P-value", ylab = "Density", axes = FALSE,
+    main = "P-value distribution", xlim = c(0, 1), ylim = c(0, 50),
+    cex.lab = 1.5, cex.main = 1.5, cex.sub = 1
+)
+curve(pdf2_t, 0, 1, n = 1000, col = "black", lty = 1, lwd = 3, add = TRUE)
+axis(side = 1, at = seq(0, 1, 0.05),
+    labels = formatC(seq(0, 1, 0.05), format = "f", digits = 2), cex.axis = 1)
+
+#| fig-cap: "Distribution of *p*-values when the null hypothesis is true."
+
+#Set number of simulations
+nSims <- 100000 #number of simulated experiments
+p <-numeric(nSims) #set up empty variable to store all simulated *p*-values
+
+for (i in 1:nSims) { # for each simulated experiment
+  x <- rnorm(n = 71, mean = 100, sd = 15) # Simulate data
+  y <- rnorm(n = 71, mean = 100, sd = 15) # Simulate data
+  p[i] <- t.test(x, y)$p.value # store the *p*-value
+}
+
+bars<-20
+#Plot figure
+op <- par(mar = c(5,7,4,4)) #change white-space around graph
+par(bg = backgroundcolor)
+hist(p, breaks=bars, xlab="P-values", ylab="number of p-values\n", axes=FALSE,
+    main=paste("P-value distribution when the null hypothesis is true"),
+    col="grey", xlim=c(0,1), ylim=c(0, nSims))
+axis(side=1, at=seq(0,1, 0.1), labels=seq(0,1,0.1))
+axis(side=2, at=seq(0,nSims, nSims/4), labels=seq(0,nSims, nSims/4), las=2)
+abline(h=nSims/bars, col = "red", lty=3)
+
+#| fig-cap: "*P*-value distribution for 0 (grey horizontal line, 50 percent power (black solid curve), and 99 percent power (black dotted curve, where *p*-values just below 0.05 are more likely when $H_0$ is true than when $H_1$ is true)."
+# Lindley plot
+
+n <- 150
+p <- 0.05
+ymax <- 25 # Maximum value y-scale (only for p-curve)
+
+# Calculations
+
+# p-value function
+pdf2_t <- function(p) 0.5 * dt(qt(p / 2, 2 * n - 2, 0), 2 * n - 2, ncp) / dt(qt(p / 2, 2 * n - 2, 0), 2 * n - 2, 0) + dt(qt(1 - p / 2, 2 * n - 2, 0), 2 * n - 2, ncp) / dt(qt(1 - p / 2, 2 * n - 2, 0), 2 * n - 2, 0)
+
+par(bg = backgroundcolor)
+plot(-10,
+  xlab = "P-value", ylab = "Density", axes = FALSE,
+  main = "P-value distribution for d = 0, 50% power, and 99% power", xlim = c(0, 1), ylim = c(0, ymax), cex.lab = 1.2, cex.main = 1.2, cex.sub = 1
+)
+axis(side = 1, at = seq(0, 1, 0.05), labels = formatC(seq(0, 1, 0.05), format = "f", digits = 2), cex.axis = 1)
+# Draw null line
+ncp <- (0 * sqrt(n / 2)) # Calculate non-centrality parameter d
+curve(pdf2_t, 0, 1, n = 1000, col = "grey", lty = 1, lwd = 2, add = TRUE)
+# Draw 50% low power line
+n <- 146
+d <- 0.23
+se <- sqrt(2 / n) # standard error
+ncp <- (d * sqrt(n / 2)) # Calculate non-centrality parameter d
+curve(pdf2_t, 0, 1, n = 1000, col = "black", lwd = 3, add = TRUE)
+# Draw 99% power line
+n <- 150
+d <- 0.5
+se <- sqrt(2 / n) # standard error
+ncp <- (d * sqrt(n / 2)) # Calculate non-centrality parameter d
+curve(pdf2_t, 0, 1, n = 1000, col = "black", lwd = 3, lty = 3, add = TRUE)
