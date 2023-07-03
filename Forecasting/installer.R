134f04b9a2fd179e3b520ff697b8a7fd5a3f44b1
Linus Fernandes <linus.fernandes@gmail.com>
2023-07-03 09:34:23 +0530

    feat: start Forecasting subproject

---
+++ Forecasting/installer.R
@@ -0,0 +1,32 @@
+#!/usr/bin/env Rscript
+
+install_programs <- function() {
+  if (!require(tsibble)) {
+    install.packages("tsibble", .libPaths()[1])
+  }
+}
+
+main <- function(argv) {
+  print(Sys.info())
+  sess <- sessionInfo()
+  print(sess)
+  platform <- ifelse(is.null(sess$running), "termux", sess$running)
+  switch(platform,
+    "termux" = {
+      Sys.setenv(R_LIBS_USER = "/data/data/com.termux/files/usr/lib/R/library")
+    },
+    `Arch Linux ARM` = {
+      Sys.setenv(R_LIBS_USER = "/usr/lib/R/library")
+    }
+  )
+  install_programs()
+  if (!is.null(warnings())) {
+    summary(warnings())
+  }
+  return(0)
+}
+# nolint end
+
+if (identical(environment(), globalenv())) {
+  suppressWarnings(quit(status = main(commandArgs(trailingOnly = TRUE))))
+}
