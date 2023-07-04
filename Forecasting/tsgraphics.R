4f1f45e81078c77d63051d9c03bbdbc4476de157
Linus Fernandes <linus.fernandes@gmail.com>
2023-07-04 09:16:13 +0530

    feat: add graphs

--- Forecasting/tsgraphics.R
+++ Forecasting/tsgraphics.R
@@ -100,3 +100,8 @@ vic_elec
 vic_elec |> gg_season(Demand, period = "day") +
   theme(legend.position = "none") +
   labs(y="MWh", title="Electricity demand: Victoria")
+vic_elec |> gg_season(Demand, period = "week") +
+  theme(legend.position = "none") +
+  labs(y="MWh", title="Electricity demand: Victoria")
+vic_elec |> gg_season(Demand, period = "year") +
+  labs(y="MWh", title="Electricity demand: Victoria")
