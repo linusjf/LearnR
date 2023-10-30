1cc9927b94347a990934270a7052c28865ad58d1
Linus Fernandes <linus.fernandes@gmail.com>
2023-10-30 14:15:39 +0530

    feat: generate distribution of 100 random graphs and compare against observed network

--- NetworkAnalysis/comm59.R
+++ NetworkAnalysis/comm59.R
@@ -6,6 +6,7 @@
 #
 # @description :
 ######################################################################
+library(ggplot2)
 # First read in igraph
 library(igraph)
 # read in the edge list from our github
@@ -58,3 +59,46 @@ plot(random_graph,
     edge.color = "grey60")
 print("Random Graph reciprocity: ")
 print(reciprocity(random_graph))
+print(triad.census(net59))
+print(triad.census(random_graph))
+
+# this creates a list with 100 spaces to store things.  We will store each result here.
+trial <- vector("list", 100)
+
+for ( i in 1:length(trial) ){
+  random_graph <- erdos.renyi.game(n = net59_n, p.or.m = net59_density, directed = TRUE)
+  # We assign to the ith space the result. So for the first iteration, it will assign the result to the first space in the list
+  trial[[i]] <- triad.census(random_graph)
+}
+
+# We can use the do.call and "rbind" functions together to combine all of the results into a matrix, where each row is one of our trials
+trial_df <- do.call("rbind", trial)
+
+colnames(trial_df) <- c("003", "012", "102", "021D", "021U", "021C", "111D", "111U", "030T", "030C", "201", "120D", "120U", "120C", "210", "300") # It is worth naming the columns too.
+
+# add in the observed results
+trial_df_w_observed <- rbind(trial_df, as.numeric(triad.census(net59)))
+
+# First, standardize all of the columns by dividing each of their values by the largest value in that column, so that each will be on a similar scale (0 to 1), we can visualize them meaningfully
+trial_df_w_observed <- as.data.frame(trial_df_w_observed)
+
+trial_df_w_observed[,1:ncol(trial_df_w_observed)] <- sapply(trial_df_w_observed[,1:length(trial_df_w_observed)], function(x) x/max(x))
+
+# Then split the observed from the simulation results
+trial_df <- as.data.frame(trial_df_w_observed[1:100,])
+observed <- as.numeric(trial_df_w_observed[101,])
+
+# Summarize the simulation results and add the observed data set back in for comparison
+summarized_stats <- data.frame(TriadType = colnames(trial_df),
+                                Means = sapply(trial_df, mean),
+                                LowerCI = sapply(trial_df, function(x) quantile(x, 0.05)),
+                                UpperCI = sapply(trial_df, function(x) quantile(x, 0.95)),
+                                Observed = observed)
+
+print(summarized_stats)
+
+ggplot(summarized_stats) +
+    geom_point(aes(x=TriadType, y=Means, colour=TriadType)) +
+    geom_errorbar(aes(x = TriadType, ymin=LowerCI, ymax=UpperCI, colour=TriadType), width=.1) +
+    geom_point(aes(x=TriadType, y=Observed, colour="Observed")) +
+    coord_flip()
